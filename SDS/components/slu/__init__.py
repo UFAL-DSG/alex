c4007dec71640b8e434616c4e4198a4c86f47f7b
Matěj Korvas <matej.korvas@gmail.com>
2012-11-03 19:29:18 +0100

    Cosmetics.
    Reniced the code, making it PEP8-compliant, fixing a few typos.

--- SDS/components/slu/__init__.py
+++ SDS/components/slu/__init__.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+# vim: set fdm=marker :
+# This code is PEP8-compliant. See http://www.python.org/dev/peps/pep-0008.
 
 __all__ = ['da', 'dailrclassifier', 'daiklrclassifier', 'templateclassifier']
 
@@ -7,26 +9,31 @@ import copy
 
 from collections import defaultdict
 
-from SDS.components.asr.utterance import Utterance, UtteranceHyp, UtteranceNBList, UtteranceConfusionNetwork
+from SDS.components.asr.utterance \
+    import Utterance, UtteranceHyp, UtteranceNBList, UtteranceConfusionNetwork
 from SDS.utils.string import split_by
 from SDS.utils.exception import SDSException, DAILRException
 
 import SDS.components.slu.da
 
+
 database = None
 
+
 class CategoryLabelDatabase:
-    """ Provides a convenient interface to a database of slot value pairs aka category labels.
+    """ Provides a convenient interface to a database of slot value pairs aka
+        category labels.
     """
+# {{{
     def __init__(self, file_name):
         self.database = {}
-        self.synonym_value_catogery = []
+        self.synonym_value_category = []
 
         if file_name:
             self.load(file_name)
 
     def __iter__(self):
-        for i in self.synonym_value_catogery:
+        for i in self.synonym_value_category:
             yield i
 
     def load(self, file_name):
@@ -39,10 +46,10 @@ class CategoryLabelDatabase:
         self.database = database
 
         self.normalise_database()
-        self.gen_synonym_value_catogery()
+        self.gen_synonym_value_category()
 
     def normalise_database(self):
-        """normalise database. E.g. split utterances into sequences of words.
+        """Normalise database. E.g. split utterances into sequences of words.
         """
         for name in self.database:
             for value in self.database[name]:
@@ -52,26 +59,28 @@ class CategoryLabelDatabase:
                     new_synonym = new_synonym.split()
                     self.database[name][value][i] = new_synonym
 
-    def gen_synonym_value_catogery(self):
+    def gen_synonym_value_category(self):
         for name in self.database:
             for value in self.database[name]:
                 for synonym in self.database[name][value]:
-                    self.synonym_value_catogery.append((synonym, value, name))
+                    self.synonym_value_category.append((synonym, value, name))
 
-        self.synonym_value_catogery.sort(
+        self.synonym_value_category.sort(
             key=lambda svc: len(svc[0]), reverse=True)
+# }}}
 
 
 class SLUPreprocessing:
-    """ Implements precessing of utterances or utterances and dialogue acts. The main purpose is to replace
-    all values in the database by its category labels (slot names) to reduce the complexity of the input
-    utterances.
+    """ Implements processing of utterances or utterances and dialogue acts.
+    The main purpose is to replace all values in the database by its category
+    labels (slot names) to reduce the complexity of the input utterances.
 
-    In addition, it implements text normalisation for SLU input, e.g. removing filler words such as
-    UHM, UM, etc. converting "I'm" into "I am", etc. Some normalisation is hard coded. However,
-    it can be updated by providing normalisation patterns.
+    In addition, it implements text normalisation for SLU input, e.g. removing
+    filler words such as UHM, UM, etc. converting "I'm" into "I am", etc. Some
+    normalisation is hard-coded. However, it can be updated by providing
+    normalisation patterns.
     """
-
+#{{{
     def __init__(self, cldb, text_normalization=None):
         self.cldb = cldb
 
@@ -88,35 +97,39 @@ class SLUPreprocessing:
                                                ]
 
     def text_normalization(self, utterance):
-        """ It normalise the input utterances (the output of an ASR engine).
+        """ It normalises the input utterances (the output of an ASR engine).
 
-        E.g. removing filler words such as UHM, UM, etc. converting "I'm" into "I am", etc.
+        E.g., it removes filler words such as UHM, UM, etc., converts "I'm"
+        into "I am", etc.
         """
-
+        #{{{
         for mapping in self.text_normalization_mapping:
             utterance = utterance.replace(mapping[0], mapping[1])
 
         return utterance
+        #}}}
 
     def values2category_labels_in_utterance(self, utterance):
-        """ Replaces all strings matching values in the database by its slot names. Since multiple slots can have
-        the same values, the algorithm produces multiple outputs.
+        """ Replaces all strings matching values in the database by their slot
+        names. Since multiple slots can have the same values, the algorithm
+        produces multiple outputs.
 
-        Returns a list of utterances with replaced database values. And it provides a dictionary with mapping
-        between category labels and the original string.
+        Returns a list of utterances with replaced database values, and
+        provides a dictionary with mapping between category labels and the
+        original string.
 
         """
-
+        #{{{
         utterance = copy.deepcopy(utterance)
 
-        catgeory_label_counter = defaultdict(int)
+        category_label_counter = defaultdict(int)
         category_labels = {}
 
         for s, value, name in self.cldb:
             if s in utterance:
-                category_label = name.upper(
-                ) + '-' + str(catgeory_label_counter[name.upper()])
-                catgeory_label_counter[name.upper()] += 1
+                category_label = name.upper() + '-' +\
+                    str(category_label_counter[name.upper()])
+                category_label_counter[name.upper()] += 1
 
                 category_labels[category_label] = (value, s)
                 utterance.replace(s, [category_label, ])
@@ -124,16 +137,19 @@ class SLUPreprocessing:
                 break
 
         return utterance, category_labels
+        #}}}
 
     def values2category_labels_in_da(self, utterance, da):
-        """ Replaces all strings matching values in the database by its slot names. Since multiple slots can have
-        the same values, the algorithm can produce multiple outputs.
+        """ Replaces all strings matching values in the database by their slot
+        names. Since multiple slots can have the same values, the algorithm can
+        produce multiple outputs.
 
-        Returns a list of utterances with replaced database values. And it provides a dictionary with mapping
-        between category labels and the original string.
+        Returns a list of utterances with replaced database values, and
+        provides a dictionary with mapping between category labels and the
+        original string.
 
         """
-
+        #{{{
         da = copy.deepcopy(da)
         utterance = copy.deepcopy(utterance)
 
@@ -147,34 +163,41 @@ class SLUPreprocessing:
                     break
 
         return utterance, da, category_labels
+        #}}}
 
     def category_labels2values_in_utterance(self, utterance, category_labels):
-        """Reverts the result of the values2category_labels_in_utterance(...) function.
+        """Reverts the result of the values2category_labels_in_utterance(...)
+        function.
 
         Returns the original utterance.
         """
-
+        #{{{
         utterance = copy.deepcopy(utterance)
 
         for cl in category_labels:
             utterance.replace([cl, ], category_labels[cl][1])
 
         return utterance
+        #}}}
 
     def category_labels2values_in_da(self, da, category_labels):
-        """Reverts the result of the values2category_labels_in_da(...) function.
+        """Reverts the result of the values2category_labels_in_da(...)
+        function.
 
         Returns the original dialogue act.
         """
+        #{{{
         da = copy.deepcopy(da)
         for dai in da.dais:
             if dai.value in category_labels:
                 dai.value = category_labels[dai.value][0]
 
         return da
+        #}}}
 
     def category_labels2values_in_nblist(self, nblist, category_labels):
-        """Reverts the result of the values2category_labels_in_da(...) function.
+        """Reverts the result of the values2category_labels_in_da(...)
+        function.
 
         Returns the converted N-best list.
         """
@@ -187,7 +210,8 @@ class SLUPreprocessing:
         return nblist
 
     def category_labels2values_in_conf_net(self, conf_net, category_labels):
-        """Reverts the result of the values2category_labels_in_da(...) function.
+        """Reverts the result of the values2category_labels_in_da(...)
+        function.
 
         Returns the converted confusion network.
         """
@@ -198,12 +222,15 @@ class SLUPreprocessing:
                 dai.value = category_labels[dai.value][0]
 
         return conf_net
+#}}}
+
 
 class SLUInterface:
-    """ Defines a prototypical interface each parser should provide for parsing.
+    """ Defines a prototypical interface each parser should provide for
+        parsing.
 
     It should be able to parse:
-      1) a utterance hypothesis(an instance of UtteranceHyp)
+      1) a utterance hypothesis (an instance of UtteranceHyp)
           - output: SLUHypothesis sub-class
 
       2) a N-best list of utterances (an instance of UtteranceNBList)
