d55d66bee6c5a4f1c563c06f8f55190a3565690a
Lukas Zilka <lukas@zilka.me>
2013-02-12 13:30:43 +0100

    added missing procname.py

---
+++ alex/utils/procname.py
@@ -0,0 +1,17 @@
+
+
+def set_proc_name(newname):
+    from ctypes import cdll, byref, create_string_buffer
+    libc = cdll.LoadLibrary('libc.so.6')
+    buff = create_string_buffer(len(newname)+1)
+    buff.value = newname
+    libc.prctl(15, byref(buff), 0, 0, 0)
+
+
+def get_proc_name():
+    from ctypes import cdll, byref, create_string_buffer
+    libc = cdll.LoadLibrary('libc.so.6')
+    buff = create_string_buffer(128)
+    # 16 == PR_GET_NAME from <linux/prctl.h>
+    libc.prctl(16, byref(buff), 0, 0, 0)
+    return buff.value
