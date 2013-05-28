c037e44ae23db34b96be556b88ff8c8c5ec38137
Lukas Zilka <lukas@zilka.me>
2013-05-28 17:37:16 +0200

    fixed handling of generic slu components

--- alex/components/slu/common.py
+++ alex/components/slu/common.py
@@ -1,9 +1,11 @@
-import autopath
+import inspect
 
+import autopath
 from alex.components.slu.base import SLUInterface, SLUPreprocessing, \
                                      CategoryLabelDatabase
 from alex.components.slu.exception import SLUException
 from alex.components.slu.dailrclassifier import DAILogRegClassifier
+
 
 def get_slu_type(cfg):
     """Get slu type from the configuration."""
@@ -16,8 +18,8 @@ def slu_factory(slu_type, cfg):
     preprocessing_cls = cfg['SLU'].get('preprocessing_cls', SLUPreprocessing)
     preprocessing = preprocessing_cls(cldb)
 
-    if isinstance(slu_type, SLUInterface):
-        slu = slu_type(preprocessing, cfg=cfg['SLU'].get(slu_type, None))
+    if inspect.isclass(slu_type) and issubclass(slu_type, SLUInterface):
+        slu = slu_type(preprocessing, cfg=cfg)
         return slu
     if slu_type == 'DAILogRegClassifier':
         slu = DAILogRegClassifier(preprocessing)
