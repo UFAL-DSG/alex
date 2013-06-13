b66b6cbe5ff6b64f89f4d273fa87c588f674d147
Ondrej Platek <ondrej.platek@seznam.cz>
2013-06-13 07:26:36 -0400

    Working. But it's based on webhub -> error DAHyp

--- alex/applications/asr_test.py
+++ alex/applications/asr_test.py
@@ -12,11 +12,30 @@ from alex.components.hub.webio import WebIO
 from alex.components.hub.vad import VAD
 from alex.components.hub.asr import ASR
 from alex.components.hub.slu import SLU
-from alex.components.hub.dm import DM
-from alex.components.hub.nlg import NLG
-from alex.components.hub.tts import TTS
 from alex.components.hub.messages import Command
 from alex.utils.config import Config
+from alex.components.slu.da import DialogueActHyp
+
+
+class debugSLU:
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def parse(self, asr_hyp):
+        return DialogueActHyp(1, "DummySLU ASR input: %s" % str(asr_hyp))
+
+
+class DummySLU(SLU):
+    '''The SLU component receives an ASR hypotheses and converts them into hypotheses about the meaning
+    of the input in the form of dialogue acts.
+
+    This class is for debgging purposes only. No functionality implemented.
+    '''
+
+    def __init__(self, cfg, commands, asr_hypotheses_in, slu_hypotheses_out):
+        SLU.__init__(self, cfg, commands, asr_hypotheses_in, slu_hypotheses_out)
+        self.slu = debugSLU(cfg)
+
 
 class WebHub(Hub):
     def __init__(self, cfg):
@@ -49,73 +68,72 @@ class WebHub(Hub):
         # used to read SLU hypotheses
         slu_hypotheses_out, slu_child_hypotheses = multiprocessing.Pipe()
 
-        # DM pipes
-        # used to send commands to DM
-        dm_commands, dm_child_commands = multiprocessing.Pipe()
-        # used to read DM actions
-        dm_actions_out, dm_child_actions = multiprocessing.Pipe()
+        # # DM pipes
+        # # used to send commands to DM
+        # dm_commands, dm_child_commands = multiprocessing.Pipe()
+        # # used to read DM actions
+        # dm_actions_out, dm_child_actions = multiprocessing.Pipe()
 
-        # NLG pipes
-        # used to send commands to NLG
-        nlg_commands, nlg_child_commands = multiprocessing.Pipe()
-        # used to read NLG output
-        nlg_text_out, nlg_child_text = multiprocessing.Pipe()
+        # # NLG pipes
+        # # used to send commands to NLG
+        # nlg_commands, nlg_child_commands = multiprocessing.Pipe()
+        # # used to read NLG output
+        # nlg_text_out, nlg_child_text = multiprocessing.Pipe()
 
-        # TTS pipes
-        # used to send commands to TTS
-        tts_commands, tts_child_commands = multiprocessing.Pipe()
+        # # TTS pipes
+        # # used to send commands to TTS
+        # tts_commands, tts_child_commands = multiprocessing.Pipe()
 
+        # command_connections = [aio_commands, vad_commands, asr_commands,
+        #                        slu_commands, dm_commands, nlg_commands,
+        #                        tts_commands]
 
-        command_connections = [aio_commands, vad_commands, asr_commands,
-                               slu_commands, dm_commands, nlg_commands,
-                               tts_commands]
+        command_connections = [aio_commands, vad_commands, asr_commands, slu_commands]
+
+        # non_command_connections = [aio_record, aio_child_record,
+        #                            aio_play, aio_child_play,
+        #                            vad_audio_out, vad_child_audio_out,
+        #                            asr_hypotheses_out, asr_child_hypotheses,
+        #                            slu_hypotheses_out, slu_child_hypotheses,
+        #                            dm_actions_out, dm_child_actions,
+        #                            nlg_text_out, nlg_child_text]
 
         non_command_connections = [aio_record, aio_child_record,
                                    aio_play, aio_child_play,
                                    vad_audio_out, vad_child_audio_out,
-                                   asr_hypotheses_out, asr_child_hypotheses,
-                                   slu_hypotheses_out, slu_child_hypotheses,
-                                   dm_actions_out, dm_child_actions,
-                                   nlg_text_out, nlg_child_text]
-
+                                   asr_hypotheses_out, asr_child_hypotheses]
 
         # create the hub components
         aio = WebIO(self.cfg, aio_child_commands, aio_child_record, aio_child_play)
         vad = VAD(self.cfg, vad_child_commands, aio_record, vad_child_audio_out)
         asr = ASR(self.cfg, asr_child_commands, vad_audio_out, asr_child_hypotheses)
-        slu = SLU(self.cfg, slu_child_commands, asr_hypotheses_out, slu_child_hypotheses)
-        dm  =  DM(self.cfg,  dm_child_commands, slu_hypotheses_out, dm_child_actions)
-        nlg = NLG(self.cfg, nlg_child_commands, dm_actions_out, nlg_child_text)
-        tts = TTS(self.cfg, tts_child_commands, nlg_text_out, aio_play)
+        slu = DummySLU(self.cfg, slu_child_commands, asr_hypotheses_out, slu_child_hypotheses)
 
         # start the hub components
         aio.start()
         vad.start()
         asr.start()
         slu.start()
-        dm.start()
-        nlg.start()
-        tts.start()
 
         # init the system
         call_back_time = -1
         call_back_uri = None
-        call_start = time.time()
 
         self.cfg['Logging']['system_logger'].session_start("@LOCAL_CALL")
         self.cfg['Logging']['system_logger'].session_system_log('config = ' + str(self.cfg))
 
-        self.cfg['Logging']['session_logger'].session_start(self.cfg['Logging']['system_logger'].get_session_dir_name())
+        self.cfg['Logging']['session_logger'].session_start(
+            self.cfg['Logging']['system_logger'].get_session_dir_name())
         self.cfg['Logging']['session_logger'].config('config = ' + str(self.cfg))
-        self.cfg['Logging']['session_logger'].header(self.cfg['Logging']["system_name"], self.cfg['Logging']["version"])
+        self.cfg['Logging']['session_logger'].header(
+            self.cfg['Logging']["system_name"], self.cfg['Logging']["version"])
         self.cfg['Logging']['session_logger'].input_source("aio")
-
 
         while 1:
             time.sleep(self.cfg['Hub']['main_loop_sleep_time'])
 
             if call_back_time != -1 and call_back_time < time.time():
-                aio_commands.send(Command('make_call(destination="%s")' % \
+                aio_commands.send(Command('make_call(destination="%s")' %
                                           call_back_uri, 'HUB', 'AIO'))
                 call_back_time = -1
                 call_back_uri = None
@@ -138,37 +156,11 @@ class WebHub(Hub):
                 command = slu_commands.recv()
                 self.cfg['Logging']['system_logger'].info(command)
 
-            if dm_commands.poll():
-                command = dm_commands.recv()
-                self.cfg['Logging']['system_logger'].info(command)
-
-                if isinstance(command, Command):
-                    if command.parsed['__name__'] == "hangup":
-                        # prepare for ending the call
-                        pass
-
-                    if command.parsed['__name__'] == "dm_da_generated":
-                        # record the time of the last system generated dialogue act
-                        pass
-
-            if nlg_commands.poll():
-                command = nlg_commands.recv()
-                self.cfg['Logging']['system_logger'].info(command)
-
-            if tts_commands.poll():
-                command = tts_commands.recv()
-                self.cfg['Logging']['system_logger'].info(command)
-
-            current_time = time.time()
-
         # stop processes
         aio_commands.send(Command('stop()', 'HUB', 'AIO'))
         vad_commands.send(Command('stop()', 'HUB', 'VAD'))
         asr_commands.send(Command('stop()', 'HUB', 'ASR'))
         slu_commands.send(Command('stop()', 'HUB', 'SLU'))
-        dm_commands.send(Command('stop()', 'HUB', 'DM'))
-        nlg_commands.send(Command('stop()', 'HUB', 'NLG'))
-        tts_commands.send(Command('stop()', 'HUB', 'TTS'))
 
         # clean connections
         for c in command_connections:
@@ -182,7 +174,6 @@ class WebHub(Hub):
         # wait for processes to stop
         aio.join()
         vad.join()
-        tts.join()
 
 
 def main():
@@ -198,7 +189,7 @@ def main():
       """)
 
     parser.add_argument('-c', action="append", dest="configs",
-        help='additional configuration file')
+                        help='additional configuration file')
     args = parser.parse_args()
 
     cfg = Config('resources/default.cfg', True)
