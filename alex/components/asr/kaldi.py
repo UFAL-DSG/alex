eac679b6668a4b5839d9e6be9235a36ddf95828b
Ondrej Platek <ondrej.platek@seznam.cz>
2013-06-14 13:19:49 -0400

    working with setting PYTHONPATH for kaldiDecoders.py

--- alex/components/asr/kaldi.py
+++ alex/components/asr/kaldi.py
@@ -4,34 +4,10 @@
 # author: Ondrej Platek
 from alex.components.asr.utterance import UtteranceNBList, UtteranceConfusionNetwork, Utterance
 from alex.components.asr.kaldiException import KaldiSetupException
-import os
-import os.path
-import sys
-
-
-'''
-Remarks
-I can generate following classes in which can be accepted by SLU
-described in ASR alex/components/slu/base.py:564
-
-    def parse(self, utterance, *args, **kw):
-        """Check what the input is and parse accordingly."""
-
-        if isinstance(utterance, Utterance):
-            return self.parse_1_best(utterance, *args, **kw)
-
-        elif isinstance(utterance, UtteranceHyp):
-            return self.parse_1_best(utterance, *args, **kw)
-
-        elif isinstance(utterance, UtteranceNBList):
-            return self.parse_nblist(utterance, *args, **kw)
-
-        elif isinstance(utterance, UtteranceConfusionNetwork):
-            return self.parse_confnet(utterance, *args, **kw)
-
-        else:
-            raise DAILRException("Unsupported input in the SLU component.")
-'''
+try:
+    from kaldiDecoders import NbListDecoder, ConfNetDecoder
+except ImportError as e:
+    raise KaldiSetupException('%s\nTry setting PYTHONPATH' % e.message)
 
 
 class KaldiASR(object):
@@ -47,15 +23,8 @@ class KaldiASR(object):
         Kcfg = cfg['ASR']['Kaldi']
         self.debug = cfg['ASR'].get('debug', False)
 
-        hyp_type = Kcfg['hypothesis_type']
-        if hyp_type == 'confnet':
-            self._hyp_out = self._decode_confnet
-        elif hyp_type == 'nblist':
-            self.hyp_out = self._decode_nblist
-        else:
-            raise KaldiSetupException("Not supported output type")
+        self.setDecoderType(Kcfg['hypothesis_type'])
 
-        self._set_path(Kcfg['ENV_SETTINGS'])
         self.model = Kcfg['model']
         self.LM_scale = Kcfg['LM_scale']
         self.lat_depth = Kcfg['lat_depth']
@@ -69,24 +38,17 @@ class KaldiASR(object):
         self.logger.info('debug:%r\nmodel:%s\nLM_scale:%f\nlat_depth:%d\n' % (
             self.debug, self.model, self.LM_scale, self.lat_depth))
 
-    def _set_path(self, d):
-        """Set the environment (LD_LIBRARY_PATH, PYTHON_PATH)
-        for running Kaldi.
-        :d: Dictionary containing system and Python variables
-        """
-        if KaldiASR._set_path_executed:
-            self.logger.debug('Setting ENVIROMENT VARIABLE SECOND time!')
+    def setDecoderType(self, hyp_type):
+        if hyp_type == 'confnet':
+            self.decoder = ConfNetDecoder()
+            self.hyp_out = self._decode_confnet
+        elif hyp_type == 'nblist':
+            self.decoder = NbListDecoder()
+            self.hyp_out = self._decode_nblist
         else:
-            for k, path in d.iteritems():
-                if not os.path.isdir(path):
-                    raise KaldiSetupException(
-                        'Cannot set %s.\n%s\nPath does not exists!\n' % (k, path))
-            sys.path.append(d['kaldi_python'])
-            llp = os.environ['LD_LIBRARY_PATH'].rstrip(':')
-            os.environ['LD_LIBRARY_PATH'] = ':'.join([llp, d['openblas'], d['openfst'], d['portaudio']])
-            self.logger.debug(os.environ['LD_LIBRARY_PATH'])
+            raise KaldiSetupException("Not supported output type")
 
-    def _decode_confnet(self, arg1):
+    def _decode_confnet(self):
         """@todo: Docstring for _decode_confnet
 
         :arg1: @todo
@@ -100,20 +62,11 @@ class KaldiASR(object):
         :returns: instance of UtteranceNBList
 
         """
+        dnb = self.decoder.Decode()
         nb = UtteranceNBList()
-
-        # FIXME
-        nb.add(1.0, Utterance('This is decoded UTTERANCE :) Testing purposes [Oplatek]!'))
-
+        for prob, hyp in dnb:
+            nb.add(prob, Utterance(hyp))
         return nb
-
-    def hyp_out(self):
-        """ This defines asynchronous interface for speech recognition.
-        It returns recognized hypothesis.
-
-        :returns: Object of type Utterance|UtteranceHyp|etc. based on self.cfg
-        """
-        return self._hyp_out()
 
     def flush(self):
         """
