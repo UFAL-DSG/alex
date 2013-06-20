603a058689fb82a85b0584b50c09519fac2eeb77
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2013-06-20 23:21:55 +0200

    If the Analytics class is constructed without any parameters, then it does not contact the GA servers.

--- alex/utils/analytics.py
+++ alex/utils/analytics.py
@@ -4,7 +4,7 @@
 from pyga.requests import Tracker, Page, Event, Session, Visitor
 
 class Analytics(object):
-    def __init__(self, account_id, domain_name):
+    def __init__(self, account_id = None, domain_name = None):
         self.account_id = account_id
         self.domain_name = domain_name
 
@@ -15,16 +15,19 @@ class Analytics(object):
     def start_session(self, caller_id):
         self.caller_id = caller_id
         
-        self.tracker = Tracker(self.account_id, self.domain_name)
-        self.visitor = Visitor()
-        self.visitor.user_agent = self.caller_id
-        self.session = Session()
+        if self.account_id:
+            self.tracker = Tracker(self.account_id, self.domain_name)
+            self.visitor = Visitor()
+            self.visitor.user_agent = self.caller_id
+            self.session = Session()
         
     def track_pageview(self, page):
-        self.tracker.track_pageview(Page(page), self.session, self.visitor)        
+        if self.account_id:
+            self.tracker.track_pageview(Page(page), self.session, self.visitor)        
         
     def track_event(self, category=None, action=None, label=None, value=None):
-        if label is None:
-            label = self.caller_id
-            
-        self.tracker.track_event(Event(category, action, label, value), self.session, self.visitor)        
+        if self.account_id:
+            if label is None:
+                label = self.caller_id
+                
+            self.tracker.track_event(Event(category, action, label, value), self.session, self.visitor)        
