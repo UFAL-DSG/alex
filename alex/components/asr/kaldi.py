331aa499ce391c9772b61548b41ea2f8f664d6c3
Ondrej Platek <ondrej.platek@seznam.cz>
2013-06-26 18:06:22 +0200

    Fix kadli import

--- alex/components/asr/kaldi.py
+++ alex/components/asr/kaldi.py
@@ -5,7 +5,7 @@
 from alex.components.asr.utterance import UtteranceNBList, UtteranceConfusionNetwork, Utterance
 from alex.components.asr.kaldiException import KaldiSetupException
 try:
-    from kaldiDecoders import NbListDecoder, ConfNetDecoder
+    from kaldi_decoders import NbListDecoder, ConfNetDecoder
 except ImportError as e:
     raise KaldiSetupException('%s\nTry setting PYTHONPATH' % e.message)
 
