e38067710015066fa7869c525c70c222791eb8b8
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2013-07-09 11:52:18 -0500

    Fixing the use in list of additional configs.

--- alex/applications/vhub.py
+++ alex/applications/vhub.py
@@ -437,8 +437,7 @@ if __name__ == '__main__':
 
       """)
 
-    parser.add_argument('-c', '--configs', nargs='+',
-                        help='additional configuration files')
+    parser.add_argument('-c', '--configs', nargs='+', help='additional configuration files')
     args = parser.parse_args()
 
     cfg = Config.load_configs(args.configs)
