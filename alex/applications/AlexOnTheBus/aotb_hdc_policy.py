3092be01640871fdd33c86ff71d93d793958a231
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2013-09-04 16:16:51 +0200

    Clean up.

--- alex/applications/AlexOnTheBus/aotb_hdc_policy.py
+++ alex/applications/AlexOnTheBus/aotb_hdc_policy.py
@@ -230,11 +230,8 @@ class AOTBHDCPolicy(DialoguePolicy):
 
             res_da.extend(req_da)
 
-            print "X0"
             if len(req_da) == 0:
-                print "X1"
                 if dialogue_state['from_stop'] == dialogue_state['to_stop']:
-                    print "X3"
                     apology_da = DialogueAct()
                     apology_da.extend(DialogueAct(u'apology()'))
                     apology_da.extend(DialogueAct(u'inform(stops_conflict="thesame")'))
@@ -244,7 +241,6 @@ class AOTBHDCPolicy(DialoguePolicy):
                         DialogueAct(u"inform(to_stop='%s')" % dialogue_state['to_stop']))
                     res_da.extend(apology_da)
                 else:
-                    print "X4"
                     dir_da = self.get_directions(dialogue_state)
                     res_da.extend(dir_da)
 
