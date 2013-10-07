47a3bed30bf7f90c46e702e881d97179b5757ea3
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2013-10-07 19:14:15 +0200

    Finished testing SLU and some refactoring.

---
+++ alex/applications/PublicTransportInfoCS/slu/test_bootstrap.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import os.path
+import codecs
+import autopath
+
+from alex.applications.PublicTransportInfoCS.preprocessing import PTICSSLUPreprocessing
+from alex.components.asr.utterance import Utterance, UtteranceNBList, UtteranceConfusionNetwork
+from alex.components.slu.dai_clser_fj_ext import CategoryLabelDatabase, DAILogRegClassifier
+from alex.corpustools.wavaskey import load_wavaskey, save_wavaskey
+from alex.corpustools.semscore import score
+
+cldb = CategoryLabelDatabase('../data/database.py')
+preprocessing = PTICSSLUPreprocessing(cldb)
+slu = DAILogRegClassifier(cldb, preprocessing)
+
+
+def test(fn_model, fn_input, constructor, fn_reference):
+    """
+    Tests a SLU DAILogRegClassifier model.
+
+    :param fn_model:
+    :param fn_input:
+    :param constructor:
+    :param fn_reference:
+    :return:
+    """
+    print "="*120
+    print "Testing: ", fn_model, fn_input, fn_reference
+    print "-"*120
+
+    slu.load_model(fn_model)
+
+    test_utterances = load_wavaskey(fn_input, constructor, limit=100000)
+
+    parsed_das = {}
+    for utt_key, utt in sorted(test_utterances.iteritems()):
+        if isinstance(utt, Utterance):
+            obs = {'utt': utt}
+        elif isinstance(utt, UtteranceNBList):
+            obs = {'utt_nbl': utt}
+        else:
+            raise BaseException('Unsupported observation type')
+
+        print '-' * 120
+        print "Observation:"
+        print utt_key, " ==> "
+        print unicode(utt)
+
+        da_confnet = slu.parse(obs, verbose=False)
+
+        print "Conf net:"
+        print unicode(da_confnet)
+
+        da_confnet.prune()
+        dah = da_confnet.get_best_da_hyp()
+
+        print "1 best: "
+        print unicode(dah)
+
+        parsed_das[utt_key] = dah.da
+
+        if 'CL_' in str(dah.da):
+            print '*' * 120
+            print utt
+            print dah.da
+            slu.parse(obs, verbose=True)
+
+    if 'trn' in fn_model:
+        fn_sem = os.path.basename(fn_input)+'.model.trn.sem.out'
+    elif 'asr' in fn_model:
+        fn_sem = os.path.basename(fn_input)+'.model.asr.sem.out'
+    elif 'nbl' in fn_model:
+        fn_sem = os.path.basename(fn_input)+'.model.nbl.sem.out'
+    else:
+        fn_sem = os.path.basename(fn_input)+'.XXX.sem.out'
+
+    save_wavaskey(fn_sem, parsed_das)
+
+    f = codecs.open(os.path.basename(fn_sem)+'.score', 'w+', encoding='UTF-8')
+    score(fn_reference, fn_sem, True, f)
+    f.close()
+
+#test('./dailogreg.trn.model', './dev.trn', Utterance, './dev.trn.hdc.sem')
+#test('./dailogreg.trn.model', './dev.asr', Utterance, './dev.trn.hdc.sem')
+#test('./dailogreg.trn.model', './dev.nbl', UtteranceNBList, './dev.trn.hdc.sem')
+#
+#test('./dailogreg.trn.model', './test.trn', Utterance, './test.trn.hdc.sem')
+#test('./dailogreg.trn.model', './test.asr', Utterance, './test.trn.hdc.sem')
+#test('./dailogreg.trn.model', './test.nbl', UtteranceNBList, './test.trn.hdc.sem')
+#
+#
+#test('./dailogreg.asr.model', './dev.trn', Utterance, './dev.trn.hdc.sem')
+#test('./dailogreg.asr.model', './dev.asr', Utterance, './dev.trn.hdc.sem')
+#test('./dailogreg.asr.model', './dev.nbl', UtteranceNBList, './dev.trn.hdc.sem')
+#
+#test('./dailogreg.asr.model', './test.trn', Utterance, './test.trn.hdc.sem')
+#test('./dailogreg.asr.model', './test.asr', Utterance, './test.trn.hdc.sem')
+#test('./dailogreg.asr.model', './test.nbl', UtteranceNBList, './test.trn.hdc.sem')
+
+#test('./dailogreg.nbl.model', './dev.trn', Utterance, './dev.trn.hdc.sem')
+#test('./dailogreg.nbl.model', './dev.asr', Utterance, './dev.trn.hdc.sem')
+test('./dailogreg.nbl.model', './dev.nbl', UtteranceNBList, './dev.trn.hdc.sem')
+#
+#test('./dailogreg.nbl.model', './test.trn', Utterance, './test.trn.hdc.sem')
+#test('./dailogreg.nbl.model', './test.asr', Utterance, './test.trn.hdc.sem')
+test('./dailogreg.nbl.model', './test.nbl', UtteranceNBList, './test.trn.hdc.sem')
---
+++ alex/corpustools/asrscore.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import re
+import argparse
+import sys
+import codecs
+
+from collections import defaultdict
+
+import autopath
+
+from alex.utils.text import split_by
+
+def load_semantics(file_name):
+    f = codecs.open(file_name,encoding = 'UTF-8')
+
+    semantics = defaultdict(list)
+    for l in f:
+        l = l.strip()
+        if not l:
+            continue
+
+        l = l.split("=>")
+
+        key = l[0].strip()
+        sem = l[1].strip()
+
+        sem = split_by(sem, '&', '(', ')', '"')
+
+        semantics[key] = sem
+    f.close()
+
+    return semantics
+
+def score_da(ref_da, test_da):
+    """Computed according to http://en.wikipedia.org/wiki/Precision_and_recall"""
+
+    tp = 0.0
+    fp = 0.0
+    fn = 0.0
+
+    statsp = defaultdict(lambda : defaultdict(float))
+
+    for i in test_da:
+        ri = re.sub(ur'([\w]+|\B)(="[\w\'!\. :]+")', r'\1="*"', i, flags=re.UNICODE)
+        if i in ref_da:
+            tp += 1.0
+            statsp[ri]['tp'] += 1.0
+        else:
+            fp += 1.0
+            statsp[ri]['fp'] += 1.0
+
+    for i in ref_da:
+        ri = re.sub(ur'([\w]+|\B)(="[\w\'!\. :]+")', r'\1="*"', i, flags=re.UNICODE)
+        if i not in test_da:
+            fn += 1.0
+            statsp[ri]['fn'] += 1.0
+
+    return tp, fp, fn, statsp
+
+def score_file(refsem, testsem):
+    tp = 0.0
+    fp = 0.0
+    fn = 0.0
+
+    stats = defaultdict(lambda : defaultdict(float))
+
+    for k in sorted(refsem):
+        tpp, fpp, fnp, statsp = score_da(refsem[k], testsem[k])
+        tp += tpp
+        fp += fpp
+        fn += fnp
+
+        for kk in statsp:
+            for kkk in statsp[kk]:
+                stats[kk][kkk] += statsp[kk][kkk]
+
+    precision = 100.0*tp/(tp+fp)
+    recall    = 100.0*tp/(tp+fn)
+
+    for k in stats:
+        try:
+            stats[k]['precision'] = 100.0*stats[k]['tp']/(stats[k]['tp']+stats[k]['fp'])
+        except ZeroDivisionError:
+            stats[k]['precision'] = 0.001
+
+        try:
+            stats[k]['recall'] = 100.0*stats[k]['tp']/(stats[k]['tp']+stats[k]['fn'])
+        except ZeroDivisionError:
+            stats[k]['recall'] = 0.001
+
+        stats[k]['precision'] += 0.000001
+        stats[k]['recall']    += 0.000001
+
+    return precision, recall, stats
+
+def score(refsem, testsem, item_level = False, outfile = sys.stdout):
+    refsem  = load_semantics(refsem)
+    testsem = load_semantics(testsem)
+
+    precision, recall, stats = score_file(refsem, testsem)
+
+    if item_level:
+        outfile.write("The results are based on {num_das} DAs\n".format(num_das=len(refsem)))
+        outfile.write("-"*80)
+        outfile.write("\n")
+
+        outfile.write("%40s %10s %10s %10s " % ('Dialogue act', 'Precision',  'Recall', 'F-measure'))
+        outfile.write("\n")
+        for k in sorted(stats):
+            outfile.write("%40s %10.2f %10.2f %10.2f " % (k,
+                                                          stats[k]['precision'],
+                                                          stats[k]['recall'],
+                                                          2*stats[k]['precision']*stats[k]['recall']/(stats[k]['precision']+stats[k]['recall'])
+            ))
+            outfile.write("\n")
+
+        outfile.write("-"*80)
+        outfile.write("\n")
+
+    outfile.write("Total precision: %6.2f" % precision)
+    outfile.write("\n")
+    outfile.write("Total recall:    %6.2f" % recall)
+    outfile.write("\n")
+    outfile.write("Total F-measure: %6.2f" % (2*precision*recall/(precision+recall), ))
+    outfile.write("\n")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description="""
+    Compute scores for semantic parser output against reference semantics.
+    The scores include total item precision and recall, and slot level
+    precision and recall.
+
+    The files structures must be as follows:
+      sem_name    => sem_content
+      ----------------------------------------
+      0000001.wav => inform(food="Chinese")
+      0000002.wav => request(phone)
+
+    The semantics from the test file and the reference file is matched
+    based on the sem_name.
+    """)
+
+    parser.add_argument('refsem', action="store", help='a file with reference semantics')
+    parser.add_argument('testsem', action="store", help='a file with tested semantics')
+    parser.add_argument('-i', action="store_true", default=False, dest="item_level", help='print item level precision and recall')
+
+    args = parser.parse_args()
+
+    score(args.refsem, args.testsem, args.item_level)
