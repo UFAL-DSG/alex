6686bd6e4716a06605590c4e92c43753597686ed
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2013-10-08 19:21:28 +0200

    Google directions can be cast to string

--- alex/applications/PublicTransportInfoCS/directions.py
+++ alex/applications/PublicTransportInfoCS/directions.py
@@ -25,12 +25,24 @@ class GoogleDirections(Directions):
     def __len__(self):
         return len(self.routes)
 
+    def __repr__(self):
+        ret = ''
+        for i, route in enumerate(self.routes, start=1):
+            ret += "ROUTE " + str(i) + "\n" + route.__repr__() + "\n\n"
+        return ret
 
-class GoogleRoute(object):
+
+class GoogleRoute(Route):
     def __init__(self, input_json):
         self.legs = []
         for leg in input_json['legs']:
             self.legs.append(GoogleRouteLeg(leg))
+
+    def __repr__(self):
+        ret = ''
+        for i, leg in enumerate(self.legs, start=1):
+            ret += "LEG " + str(i) + "\n" + leg.__repr__() + "\n"
+        return ret
 
 
 class GoogleRouteLeg(object):
@@ -38,6 +50,9 @@ class GoogleRouteLeg(object):
         self.steps = []
         for step in input_json['steps']:
             self.steps.append(GoogleRouteLegStep(step))
+
+    def __repr__(self):
+        return "\n".join(step.__repr__() for step in self.steps)
 
 
 class GoogleRouteLegStep(object):
@@ -71,7 +86,15 @@ class GoogleRouteLegStep(object):
             hour = (hour + 12) % 24
         return datetime.combine(datetime.now(),
                                 dttime(hour, mins))
-
+    def __repr__(self):
+        ret = self.travel_mode
+        if self.travel_mode == self.MODE_TRANSIT:
+            ret += ': ' + self.vehicle + ' ' + self.line_name + \
+                    ' [^' + self.headsign + ']: ' + self.departure_stop + \
+                    ' ' + str(self.departure_time) + ' -> '  + \
+                    self.arrival_stop + ' ' + str(self.arrival_time)
+            
+        return ret.encode('utf8')
 
 class DirectionsFinder(object):
     def get_directions(self, from_stop, to_stop, time):
@@ -84,6 +107,7 @@ class GooglePIDDirectionsFinder(DirectionsFinder):
         self.directions_url = 'http://maps.googleapis.com/maps/api/directions/json'
 
     def get_directions(self, from_stop, to_stop, departure_time):
+
         departure = datetime.combine(
             datetime.now(),
             datetime.strptime(departure_time, "%H:%M").time()
