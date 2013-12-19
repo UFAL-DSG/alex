5acef4570331b3052d8f1ce6c49b48779c56a331
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2013-12-19 20:55:53 +0100

    Improvements to the Spanish version.

--- alex/applications/Switchboard/sw_hub_es.sh
+++ alex/applications/Switchboard/sw_hub_es.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./sw_hub.py -c sw_hub_es.cfg
+./sw_hub.py -o sw_hub_es1.cfg ../../resources/private/ext-sw-277278111.cfg -d sw_hub_es2.cfg ../../resources/private/ext-sw-277278112.cfg
 
