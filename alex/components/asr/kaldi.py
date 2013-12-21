5665d1b749e5acf1c300f07de60cea43f27e4e15
Ondrej Platek <ondrej.platek@seznam.cz>
2013-12-21 15:52:08 +0100

    Kaldi decoder running on ptics with en LM
    Run alex/applications/PublicTransportInfoCs/vhub_test_kaldi

--- alex/components/asr/kaldi.py
+++ alex/components/asr/kaldi.py
@@ -11,6 +11,8 @@ except ImportError as e:
     # FIXME PYTHONPATH I can change : sys.path insert into(0,)
     raise KaldiSetupException('%s\nTry setting PYTHONPATH or LD_LIBRARY_PATH' % e.message)
 import time
+from datetime import datetime
+import os
 
 
 class KaldiASR(object):
@@ -25,6 +27,8 @@ class KaldiASR(object):
         self.logger = cfg['Logging']['system_logger']
         self.cfg = cfg
         kcfg = cfg['ASR']['Kaldi']
+
+        self.debug = kcfg['debug']
         self.wst = wst2dict(kcfg['wst'])
         self.max_dec_frames = kcfg['max_dec_frames']
         # specify all other options in config
@@ -37,7 +41,6 @@ class KaldiASR(object):
 
         self.decoder = PyGmmLatgenWrapper()
         self.decoder.setup(argv)
-        self.decoder.reset(keep_buffer_data=False)  # FIXME is it necessary?
 
     def flush(self):
         """
@@ -56,14 +59,16 @@ class KaldiASR(object):
         :frame: @todo
         :returns: self - The instance of KaldiASR
         """
-        start = time.clock()
+        frame_total, start = 0, time.clock()
         self.decoder.frame_in(frame.payload)
-        self.logger.info('frame_in of %d frames' % (len(frame.payload) / 2))
+        self.logger.debug('frame_in of %d frames' % (len(frame.payload) / 2))
         dec_t = self.decoder.decode(max_frames=self.max_dec_frames)
         while dec_t > 0:
+            frame_total += dec_t
             dec_t = self.decoder.decode(max_frames=self.max_dec_frames)
-        self.logger.info('Forward decoding of %d frames in %s secs' % (
-            dec_t, str(time.clock() - start)))
+        if (frame_total > 0):
+            self.logger.debug('Forward decoding of %d frames in %s secs' % (
+                frame_total, str(time.clock() - start)))
         return self
 
     def hyp_out(self):
@@ -71,16 +76,29 @@ class KaldiASR(object):
         Returns recognizers hypotheses about the input speech audio.
         """
         start = time.clock()
+
+        # Get hypothesis
         self.decoder.prune_final()
-        lat = self.decoder.get_lattice()
+        utt_prob, lat = self.decoder.get_lattice()
+        self.decoder.reset(keep_buffer_data=False)
+
+        # Convert lattice to nblist
         nbest = lattice_to_nbest(lat, n=5)
         nblist = UtteranceNBList()
         for w, word_ids in nbest:
-            words = [self.wst[str(i)] for i in word_ids]
+            words = ' '.join([self.wst[str(i)] for i in word_ids])
             nblist.add(w, Utterance(words))
-        self.logger.info('hyp_out: get_lattice+nbest in %s secs' % str(time.clock() - start))
+
+        # Log
         if len(nbest) == 0:
             self.logger.warning('hyp_out: empty hypothesis')
             nblist.add(1.0, Utterance('Empty hypothesis: DEBUG'))
+        if self.debug:
+            output_file_name = os.path.join(
+                self.logger.get_session_dir_name(),
+                '%s.fst' % str(datetime.now().strftime('%Y-%m-%d-%H-%M-%S.%f')))
+            lat.write(output_file_name)
+        self.logger.info('utterance "probability" is %f' % utt_prob)
+        self.logger.debug('hyp_out: get_lattice+nbest in %s secs' % str(time.clock() - start))
 
         return nblist
