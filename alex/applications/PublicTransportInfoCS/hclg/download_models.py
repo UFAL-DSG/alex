666851cfb6a3c34b9eaac6ef1e17387e9239d875
Ondrej Platek <ondrej.platek@seznam.cz>
2014-01-27 18:24:34 +0100

    fixed the target of build_hclg for download to models

---
+++ alex/applications/PublicTransportInfoCS/hclg/download_models.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import autopath
+
+from alex.utils.config import online_update
+
+online_update('applications/PublicTransportInfoCS/hclg/models/HCLG.fst')
+online_update('applications/PublicTransportInfoCS/hclg/models/final.mdl')
+online_update('applications/PublicTransportInfoCS/hclg/models/words.txt')
+online_update('applications/PublicTransportInfoCS/hclg/models/silence.csl')
