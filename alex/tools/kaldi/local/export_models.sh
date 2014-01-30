cff17b112aa02f8d74b3dcf52a6699ca7998609a
Ondrej Platek <ondrej.platek@seznam.cz>
2014-01-30 19:00:48 +0100

    fix export_models

--- alex/tools/kaldi/local/export_models.sh
+++ alex/tools/kaldi/local/export_models.sh
@@ -27,7 +27,7 @@ echo "--- Exporting models to $tgt ..."
 cp -f $exp/alex_gitlog.log $exp/alex_gitdiff.log $exp/experiment_bash_vars.log $tgt
 
 # Store also the results
-local/results.py $exp >  $tgt/results.log
+cp -f $exp/results.log >  $tgt/results.log 2> /dev/null
 
 
 cp -f common/mfcc.conf $tgt 
