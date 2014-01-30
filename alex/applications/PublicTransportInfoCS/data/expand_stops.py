e6ac0c73042a245e257c8f9e3105ed260bbbb5ca
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2014-01-30 12:20:20 +0100

    A rewrite of expand_stops, using MorphoDiTa

--- alex/applications/PublicTransportInfoCS/data/expand_stops.py
+++ alex/applications/PublicTransportInfoCS/data/expand_stops.py
@@ -1,159 +1,165 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Expanding Czech stop/city names with inflection forms in all given morphological cases.
+
+Usage:
+
+./expand_stops.py [-c 1,2,4] [-m previous.expanded.txt] stops.txt stops.expanded.txt
+
+-c = Required morphological cases (possible values: numbers 1-7, comma-separated).
+        Defaults to 1,2,4 (nominative, genitive, accusative).
+-m = Add previously expanded stops. Only stop name variants NOT found in the previously
+        expanded file are expanded to speed up the process and preserve hand-written forms.
+"""
+
+from __future__ import unicode_literals
 import sys
-import os
+import re
 from collections import defaultdict
 import itertools
+import codecs
+from ufal.morphodita import Tagger, Forms, TaggedLemmas, TokenRanges, Morpho, TaggedLemmasForms
+from alex.utils.various import remove_dups_stable
+from getopt import getopt
 
-import autopath
+stderr = codecs.getwriter('UTF-8')(sys.stderr)
 
-from alex.utils.config import as_project_path
-from alex.components.nlg.tectotpl.core.run import Scenario
+class Analyzer(object):
+    """Morphodita analyzer/tagger wrapper."""
+
+    def __init__(self, tagger_model):
+        self.__tagger = Tagger.load(tagger_model)
+        self.__tokenizer = self.__tagger.newTokenizer()
+        self.__forms_buf = Forms()
+        self.__tokens_buf = TokenRanges()
+        self.__lemmas_buf = TaggedLemmas()
+
+    def analyze(self, stop_text):
+        self.__tokenizer.setText(stop_text)
+        while self.__tokenizer.nextSentence(self.__forms_buf, self.__tokens_buf):
+            self.__tagger.tag(self.__forms_buf, self.__lemmas_buf)
+            return [(form, lemma.lemma, lemma.tag)
+                    for (form, lemma) in zip(self.__forms_buf, self.__lemmas_buf)]
 
 
+class Generator(object):
+    """Morphodita generator wrapper."""
 
+    def __init__(self, morpho_model):
+        self.__morpho = Morpho.load(morpho_model)
+        self.__out_buf = TaggedLemmasForms()
 
-TECTO_CFG = {
-            'scenario': [
-                {'block': 'read.TectoTemplates', 'args': {'encoding': None}},
-                {'block': 't2a.CopyTTree'},
-                {'block': 't2a.cs.ReverseNumberNounDependency'},
-                {'block': 't2a.cs.InitMorphcat'},
-                {'block': 't2a.cs.GeneratePossessiveAdjectives'},
-                {'block': 't2a.cs.MarkSubject'},
-                {'block': 't2a.cs.ImposePronZAgr'},
-                {'block': 't2a.cs.ImposeRelPronAgr'},
-                {'block': 't2a.cs.ImposeSubjPredAgr'},
-                {'block': 't2a.cs.ImposeAttrAgr'},
-                {'block': 't2a.cs.ImposeComplAgr'},
-                {'block': 't2a.cs.DropSubjPersProns'},
-                {'block': 't2a.cs.AddPrepositions'},
-                {'block': 't2a.cs.AddSubconjs'},
-                {'block': 't2a.cs.GenerateWordForms', 'args': {'model': 'flect/model-t253-l1_10_00001-alex.pickle.gz'}},
-                {'block': 't2a.cs.VocalizePrepos'},
-                {'block': 't2a.cs.CapitalizeSentStart'},
-                {'block': 'a2w.cs.ConcatenateTokens'},
-                {'block': 'a2w.cs.RemoveRepeatedTokens'},
-            ],
-            'global_args': {'language': 'cs', 'selector': ''},
-            'data_dir': as_project_path('applications/TectoTplTest/data/'),
-}
+    def generate(self, lemma, tag_wildcard, capitalized=None):
+        # run the generation for this word
+        self.__morpho.generate(lemma, tag_wildcard, self.__morpho.GUESSER, self.__out_buf)
+        # see if we found any forms, return empty if not
+        if not self.__out_buf:
+            return []
+        # prepare capitalization
+        cap_func = lambda string: string
+        if capitalized == True:
+            cap_func = lambda string: string[0].upper() + string[1:]
+        elif capitalized == False:
+            cap_func = lambda string: string[0].lower() + string[1:]
+        # process the results
+        return [cap_func(form_tag.form) for form_tag in self.__out_buf[0].forms]
 
 
 class ExpandStops(object):
-    tpl = u"[{stop}|n:{case}|gender:{gender},number:{number}]"
-    tpl_adj = u"[{stop}|adj:attr]"
+    """This handles inflecting stop names into all desired cases in Czech."""
 
-    def __init__(self):
+    def __init__(self, cases_list):
+        """Initialize the expander object, initialize the morphological analyzer and generator.
+
+        @param cases_list: List of cases (given as strings) to be used for generation \
+                (Czech numbers 1-7 are used)
+        """
         self.stops = defaultdict(list)
-
-
-    def expand(self):
-        scen = Scenario(TECTO_CFG)
-        scen.load_blocks()
-
-        for stop in self.stops.keys():
-            for gender, case, number in itertools.product(['fem', 'anim', 'inan', 'neut'], [1, 2, 3, 4, 6, 7], ["sg", "pl"]):
-                print gender, case, number
-                stop_f = self.tpl.format(stop=stop, gender=gender, case=case, number=number)
-                stop_out = scen.apply_to(stop_f)
-                self.stops[stop].append(stop_out)
-                print stop, stop_out
-
-    def expand_morfflex(self):
-        morf_dict = defaultdict(set)
-        with open('/tmp/morfflex-cz.2013-05-02.utf8.lemmaID_suff-tag-form.tab.csv') as f_in:
-            curr_word = None
-            curr_buff = []
-            curr_in = False
-            def curr_save():
-                if curr_in:
-                    for t in curr_buff:
-                        morf_dict[t].add(curr_word)
-                        morf_dict[curr_word].add(t)
-
-            for i, ln in enumerate(f_in):
-                t1, _, t2 = ln.strip().lower().decode('utf8').split('\t')
-                t1x = t1.split('-')[0]
-                t1x = t1x.split('_')[0]
-                if t1x != curr_word:
-                    curr_save()
-                    curr_word = t1x
-                    curr_buff = []
-                    curr_in = False
-
-                curr_buff.append(t2)
-
-                if t2 in self.stops_words:
-                    curr_in = True
-
-                #if i > 1000000:
-                #    break
-                #print i
-            curr_save()
-
-        # save morf dict
-
-        with open('/tmp/morf_dict', 'w') as f_out:
-            for key, values in morf_dict.iteritems():
-                f_out.write(key.encode('utf8') + " : ")
-                f_out.write(";".join(values).encode('utf8'))
-                f_out.write("\n")
-
-        for stop in self.stops.keys():
-            res = []
-            stop_s = stop.lower().split()
-
-            #print stop_s
-            #print morf_dict.items()[:10]
-
-
-            for part in stop_s:
-                res.append(morf_dict.get(part, set([part])))
-                #print len(morf_dict.get(part, set([part])))
-
-
-
-            #print res
-            for word in itertools.product(*res):
-                stop_out = " ".join(word)
-                self.stops[stop].append(stop_out)
-                print stop_out
-
+        self.cases_list = cases_list
+        self.__analyzer = Analyzer('/home/odusek/work/tools/morphodita/models/czech-131023.tagger-best_accuracy')
+        self.__generator = Generator('/home/odusek/work/tools/morphodita/models/czech-131023.dict')
 
     def save(self, fname):
-        with open(fname, "w") as f_out:
-            for key, values in self.stops.iteritems():
-                f_out.write(";".join(values).encode('utf8'))
+        """Save all stops currently held in memory to a file."""
+        with codecs.open(fname, 'w', 'UTF-8') as f_out:
+            for stop_name in sorted(self.stops.keys()):
+                f_out.write('; '.join(self.stops[stop_name]))
                 f_out.write("\n")
 
-    def parse_stops(self):
-        self.stops_words = set()
-        for stop in self.stops.keys():
-            stop_s = stop.split()
-            self.stops_words.update(set(stop_s))
+    def load_file(self, fname):
+        """Just load a list of stops from a file and store it in memory."""
+        with codecs.open(fname, 'r', 'UTF-8') as f_in:
+            for line in f_in:
+                variants = [var.strip() for var in line.split(';')]
+                self.stops[variants[0]] = list(remove_dups_stable(variants + self.stops[variants[0]]))
 
+    def expand_file(self, fname):
+        """Load a list of stops from a file and expand it."""
+        with codecs.open(fname, 'r', 'UTF-8') as f_in:
+            ctr = 0
+            for line in f_in:
+                # load variant names for a stop
+                variants = [var.strip() for var in line.split(';')]
+                # skip those that needn't be inflected any more
+                to_inflect = [var for var in variants if not var in self.stops[variants[0]]]
+                # inflect the rest
+                for variant in to_inflect:
+                    words = self.__analyzer.analyze(variant)
+                    # in all required cases
+                    for case in self.cases_list:
+                        forms = []
+                        prev_tag = ''
+                        for word in words:
+                            forms.append(self.__inflect_word(word, case, prev_tag))
+                            prev_tag = word[2]
+                        # use all possible combinations if there are more variants for this case
+                        inflected = map(lambda var: re.sub(r' ([\.,])', r'\1', var),
+                                        remove_dups_stable([' '.join(var)
+                                                            for var in itertools.product(*forms)]))
+                        self.stops[variants[0]] = list(remove_dups_stable(self.stops[variants[0]] +
+                                                                          inflected))
+                ctr += 1
+                if ctr % 100 == 0:
+                    print >> sys.stderr, '.'
 
-
-    @classmethod
-    def load_from_file(cls, fname):
-        es = ExpandStops()
-        with open(fname) as f_in:
-            for ln in f_in:
-                es.stops[ln.decode('utf8').strip()] = []
-
-        es.parse_stops()
-
-        return es
+    def __inflect_word(self, word, case, prev_tag):
+        """Inflect one word in the given case (return a list of variants)."""
+        form, lemma, tag = word
+        # inflect each word in nominative not following a noun in nominative
+        # (if current case is not nominative)
+        if re.match(r'^....1', tag) and not re.match(r'^NN..1', prev_tag) and case != '1':
+            # change the case in the tag, allow all variants
+            new_tag = re.sub(r'^(....)1(.*).$', r'\g<1>' + case + r'\g<2>?', tag)
+            # -ice: test both sg. and pl. versions
+            if form.endswith('ice'):
+                new_tag = re.sub(r'^(...)S', r'\1[SP]', new_tag)
+            # try inflecting, fallback to uninflected
+            capitalized = form[0] == form[0].upper()
+            new_forms = self.__generator.generate(lemma, new_tag, capitalized)
+            return new_forms if new_forms else [form]
+        else:
+            return [form]
 
 
 def main():
-    fname = sys.argv[1]
-    fname_out = sys.argv[2]
-    es = ExpandStops.load_from_file(fname)
-    #es.expand()
-    es.expand_morfflex()
-    es.save(fname_out)
-
+    cases = ['1', '2', '4']
+    merge_file = None
+    opts, files = getopt(sys.argv[1:], 'c:m:')
+    for opt, arg in opts:
+        if opt == '-c':
+            cases = re.split('[, ]+', arg)
+        elif opt == '-m':
+            merge_file = arg
+    if len(files) != 2:
+        sys.exit(__doc__)
+    in_file, out_file = files
+    es = ExpandStops(cases)
+    if merge_file:
+        es.load_file(merge_file)
+    es.expand_file(in_file)
+    es.save(out_file)
 
 
 if __name__ == '__main__':
