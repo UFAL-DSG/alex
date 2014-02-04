e09d6458a840ea4558ca8bdc9545430f85437431
Ondrej Platek <ondrej.platek@seznam.cz>
2014-02-05 00:58:42 +0100

    Removed custom PYTHONPATH for kaldi

--- alex/applications/PublicTransportInfoCS/hclg/run_decode_indomain_kaldi.sh
+++ alex/applications/PublicTransportInfoCS/hclg/run_decode_indomain_kaldi.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-pykaldi_dir=/ha/work/people/oplatek/kaldi/src/pykaldi
-export LD_LIBRARY_PATH=$pykaldi_dir:$LD_LIBRARY_PATH
-export PYTHONPATH=$pykaldi_dir:$pykaldi_dir/pyfst:$PYTHONPATH
-
 python download_models.py
 
 time ./decode_indomain.py -c kaldi.cfg -f -o decoded_kaldi  load ../lm/reference_transcription_dev.txt
