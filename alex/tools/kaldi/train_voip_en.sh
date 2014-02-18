0b0bc10c715e46b31a4648bb4fe76ef75df96be6
Ondrej Platek <ondrej.platek@seznam.cz>
2014-02-18 21:26:01 +0100

    finish run-cmn -> --fake cmvn stats

--- alex/tools/kaldi/train_voip_en.sh
+++ alex/tools/kaldi/train_voip_en.sh
@@ -37,7 +37,8 @@ echo "Create MFCC features and storing them (Could be large)."
 for s in train $TEST_SETS ; do
     steps/make_mfcc.sh --mfcc-config common/mfcc.conf --cmd \
       "$train_cmd" --nj $njobs $WORK/local/$s $EXP/make_mfcc/$s $WORK/mfcc || exit 1;
-    # Note --fake -> NO CMVN steps/compute_cmvn_stats.sh --fake $WORK/local/$s \
+    # Note --fake -> NO CMVN
+    steps/compute_cmvn_stats.sh --fake $WORK/local/$s \
       $EXP/make_mfcc/$s $WORK/mfcc || exit 1;
 done
 
@@ -59,42 +60,42 @@ done
 #######################################################################
 
 echo "Train monophone models on full data -> may be wastefull (can be done on subset)"
-steps/train_mono.sh --run-cmn $cmn --nj $njobs --cmd "$train_cmd" $WORK/train $WORK/lang $EXP/mono || exit 1;
+steps/train_mono.sh  --nj $njobs --cmd "$train_cmd" $WORK/train $WORK/lang $EXP/mono || exit 1;
 
 echo "Get alignments from monophone system."
-steps/align_si.sh --run-cmn $cmn --nj $njobs --cmd "$train_cmd" \
+steps/align_si.sh  --nj $njobs --cmd "$train_cmd" \
   $WORK/train $WORK/lang $EXP/mono $EXP/mono_ali || exit 1;
 
 echo "Train tri1 [first triphone pass]"
-steps/train_deltas.sh --run-cmn $cmn --cmd "$train_cmd" \
+steps/train_deltas.sh  --cmd "$train_cmd" \
   $pdf $gauss $WORK/train $WORK/lang $EXP/mono_ali $EXP/tri1 || exit 1;
 
 # draw-tree $WORK/lang/phones.txt $EXP/tri1/tree | dot -Tsvg -Gsize=8,10.5  > graph.svg
 
 echo "Align tri1"
-steps/align_si.sh --run-cmn $cmn --nj $njobs --cmd "$train_cmd" \
+steps/align_si.sh  --nj $njobs --cmd "$train_cmd" \
   --use-graphs true $WORK/train $WORK/lang $EXP/tri1 $EXP/tri1_ali || exit 1;
 
 echo "Train tri2a [delta+delta-deltas]"
-steps/train_deltas.sh --run-cmn $cmn --cmd "$train_cmd" $pdf $gauss \
+steps/train_deltas.sh  --cmd "$train_cmd" $pdf $gauss \
   $WORK/train $WORK/lang $EXP/tri1_ali $EXP/tri2a || exit 1;
 
 echo "Train tri2b [LDA+MLLT]"
-steps/train_lda_mllt.sh --run-cmn $cmn --cmd "$train_cmd" $pdf $gauss \
+steps/train_lda_mllt.sh  --cmd "$train_cmd" $pdf $gauss \
   $WORK/train $WORK/lang $EXP/tri1_ali $EXP/tri2b || exit 1;
 
 echo "Align all data with LDA+MLLT system (tri2b)"
-steps/align_si.sh --run-cmn $cmn --nj $njobs --cmd "$train_cmd" \
+steps/align_si.sh  --nj $njobs --cmd "$train_cmd" \
     --use-graphs true $WORK/train $WORK/lang $EXP/tri2b $EXP/tri2b_ali || exit 1;
 
 echo "Train MMI on top of LDA+MLLT."
-steps/make_denlats.sh --run-cmn $cmn --nj $njobs --cmd "$train_cmd" \
+steps/make_denlats.sh  --nj $njobs --cmd "$train_cmd" \
    --beam $mmi_beam --lattice-beam $mmi_lat_beam \
    $WORK/train $WORK/lang $EXP/tri2b $EXP/tri2b_denlats || exit 1;
-steps/train_mmi.sh --run-cmn $cmn $WORK/train $WORK/lang $EXP/tri2b_ali $EXP/tri2b_denlats $EXP/tri2b_mmi || exit 1;
+steps/train_mmi.sh  $WORK/train $WORK/lang $EXP/tri2b_ali $EXP/tri2b_denlats $EXP/tri2b_mmi || exit 1;
 
 echo "Train MMI on top of LDA+MLLT with boosting. train_mmi_boost is a e.g. 0.05"
-steps/train_mmi.sh --run-cmn $cmn --boost ${train_mmi_boost} $WORK/train $WORK/lang \
+steps/train_mmi.sh  --boost ${train_mmi_boost} $WORK/train $WORK/lang \
    $EXP/tri2b_ali $EXP/tri2b_denlats $EXP/tri2b_mmi_b${train_mmi_boost} || exit 1;
 
 echo "Train MPE."
@@ -124,32 +125,32 @@ for s in $TEST_SETS ; do
     # calls the command line once for each test,
     # and afterwards averages the WERs into (in this case $EXP/mono/decode/)
     steps/decode.sh --scoring-opts "--min-lmw $min_lmw --max-lmw $max_lmw" \
-      --run-cmn $cmn --config common/decode.conf --nj $njobs --cmd "$decode_cmd" \
+       --config common/decode.conf --nj $njobs --cmd "$decode_cmd" \
       $EXP/mono/graph_${lm} $WORK/${tgt_dir} $EXP/mono/decode_${tgt_dir}
     echo "Decode tri1"
     steps/decode.sh --scoring-opts "--min-lmw $min_lmw --max-lmw $max_lmw" \
-      --run-cmn $cmn --config common/decode.conf --nj $njobs --cmd "$decode_cmd" \
+       --config common/decode.conf --nj $njobs --cmd "$decode_cmd" \
       $EXP/tri1/graph_${lm} $WORK/$tgt_dir $EXP/tri1/decode_${tgt_dir}
     echo "Decode tri2a"
     steps/decode.sh --scoring-opts "--min-lmw $min_lmw --max-lmw $max_lmw" \
-      --run-cmn $cmn --config common/decode.conf --nj $njobs --cmd "$decode_cmd" \
+       --config common/decode.conf --nj $njobs --cmd "$decode_cmd" \
       $EXP/tri2a/graph_${lm} $WORK/$tgt_dir $EXP/tri2a/decode_${tgt_dir}
     echo "Decode tri2b [LDA+MLLT]"
     steps/decode.sh --scoring-opts "--min-lmw $min_lmw --max-lmw $max_lmw" \
-      --run-cmn $cmn --config common/decode.conf --nj $njobs --cmd "$decode_cmd" \
+       --config common/decode.conf --nj $njobs --cmd "$decode_cmd" \
       $EXP/tri2b/graph_${lm} $WORK/$tgt_dir $EXP/tri2b/decode_${tgt_dir}
     # Note: change --iter option to select the best model. 4.mdl == final.mdl
     echo "Decode MMI on top of LDA+MLLT."
     steps/decode.sh --scoring-opts "--min-lmw $min_lmw --max-lmw $max_lmw" \
-      --run-cmn $cmn --config common/decode.conf --iter 4 --nj $njobs --cmd "$decode_cmd" \
+       --config common/decode.conf --iter 4 --nj $njobs --cmd "$decode_cmd" \
       $EXP/tri2b/graph_${lm} $WORK/$tgt_dir $EXP/tri2b_mmi/decode_it4_${tgt_dir}
     echo "Decode MMI on top of LDA+MLLT with boosting. train_mmi_boost is a number e.g. 0.05"
     steps/decode.sh --scoring-opts "--min-lmw $min_lmw --max-lmw $max_lmw" \
-      --run-cmn $cmn --config common/decode.conf --iter 4 --nj $njobs --cmd "$decode_cmd" \
+       --config common/decode.conf --iter 4 --nj $njobs --cmd "$decode_cmd" \
       $EXP/tri2b/graph_${lm} $WORK/$tgt_dir $EXP/tri2b_mmi_b${train_mmi_boost}/decode_it4_${tgt_dir};
     echo "Decode MPE."
     steps/decode.sh --scoring-opts "--min-lmw $min_lmw --max-lmw $max_lmw" \
-      --run-cmn $cmn --config common/decode.conf --iter 4 --nj $njobs --cmd "$decode_cmd" \
+       --config common/decode.conf --iter 4 --nj $njobs --cmd "$decode_cmd" \
       $EXP/tri2b/graph_${lm} $WORK/$tgt_dir $EXP/tri2b_mpe/decode_it4_${tgt_dir} || exit 1;
   done
 done
