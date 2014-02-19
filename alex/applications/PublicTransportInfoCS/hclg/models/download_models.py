f3ba2254c1c9e7e7d15c442e49e2d338073e652b
Ondrej Platek <ondrej.platek@seznam.cz>
2014-02-19 10:00:36 +0100

    updating path in default.cfg and refactorings

--- alex/applications/PublicTransportInfoCS/hclg/models/download_models.py
+++ alex/applications/PublicTransportInfoCS/hclg/models/download_models.py
@@ -5,6 +5,5 @@ import autopath
 from alex.utils.config import online_update
 
 online_update('applications/PublicTransportInfoCS/hclg/models/HCLG_tri2b_bmmi.fst')
-#online_update('applications/PublicTransportInfoCS/hclg/models/tri2b_bmmi.mdl')
 online_update('applications/PublicTransportInfoCS/hclg/models/words.txt')
 online_update('applications/PublicTransportInfoCS/hclg/models/silence.csl')
