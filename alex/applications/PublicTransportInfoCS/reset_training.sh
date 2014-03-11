d6f59388b6e5247c857a15943defab315fbbae68
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2014-03-11 13:39:48 +0100

    Moving old scores

--- alex/applications/PublicTransportInfoCS/reset_training.sh
+++ alex/applications/PublicTransportInfoCS/reset_training.sh
@@ -11,3 +11,8 @@ cat .gitignore | xargs echo rm
 
 cd ../slu
 cat .gitignore | xargs echo rm
+
+curdate=`date +%Y%d%m-%H%M%S`
+for file in training-log* SLU-* Goog-* qsubmit.* scores-*; do
+    mv $file old.$curdate.$file
+done
