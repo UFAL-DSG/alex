180d4cd70a597c5ec8fdb766405e7849ba787db1
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2014-03-11 14:07:26 +0100

    Fixes in training reset script

--- alex/applications/PublicTransportInfoCS/reset_training.sh
+++ alex/applications/PublicTransportInfoCS/reset_training.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-cd data
-cat .gitignore | xargs echo rm
+echo -n "This will delete all intermediate files from data/ lm/, hclg/, slu/. Are you sure you want to proceed (Y/N)? "
+read -n 1 ANSWER
+if echo "$ANSWER" | grep '^[yY]' ; then
+    echo -e "\nOK."
+else
+    echo -e "\nInterrupting."
+    exit 1
+fi
 
-cd ../lm
-cat .gitignore | xargs echo rm
+echo "Saving last scores..."
+cd slu/
+./print_scores.sh >> ../scores-last.txt 2>&1
+cd ..
 
-cd ../hclg
-cat .gitignore | xargs echo rm
+echo "Deleting files..."
+clear_dir(){
 
-cd ../slu
-cat .gitignore | xargs echo rm
+    cd $1
+    TODEL=`cat .gitignore | sed 's/\n/ /g'`
+    /bin/ls -d $TODEL 2> /dev/null | grep -vE '^(stops-add.txt|models|train.py|test.py)$' | xargs rm -r
+    cd ..
+}
 
+clear_dir data
+clear_dir lm
+clear_dir hclg
+clear_dir slu
+
+echo "Moving logs..."
 curdate=`date +%Y%d%m-%H%M%S`
 for file in training-log* SLU-* Goog-* qsubmit.* scores-*; do
-    mv $file old.$curdate.$file
+    if [ -e "$file" ]; then
+        mv "$file" "old.$curdate.$file"
+    fi
 done
