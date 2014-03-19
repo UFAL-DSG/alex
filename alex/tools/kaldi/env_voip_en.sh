ecda5590f473515dc8adccb905b4068934edcce9
Ondrej Platek <ondrej.platek@seznam.cz>
2014-03-19 19:01:31 +0100

    change KALDI_ROOT PATH

--- alex/tools/kaldi/env_voip_en.sh
+++ alex/tools/kaldi/env_voip_en.sh
@@ -6,7 +6,7 @@ export EVERY_N=1
 export TEST_SETS="dev test"
 
 # Directories set up
-export KALDI_ROOT=/ha/projects/vystadial/lib/pykaldi-kronos-build
+export KALDI_ROOT=/ha/projects/vystadial/lib/kronos/pykaldi
 export DATA_ROOT=`pwd`/data_voip_en  # expects subdirectories train + $TEST_SETS
 export WORK=`pwd`/model_voip_en
 export EXP=`pwd`/model_voip_en/exp
