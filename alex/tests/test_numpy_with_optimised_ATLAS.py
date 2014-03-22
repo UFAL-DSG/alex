fc86a29ec440a51eb9335c3679629b43d59e762e
Ondrej Platek <ondrej.platek@seznam.cz>
2014-03-22 18:39:56 +0100

    docs fixes
    Using os.walk for collecting rst files
    Ignoring rst files in alex/doc/_man_rst
    Wrapping test_numpy in ifmain

--- alex/tests/test_numpy_with_optimised_ATLAS.py
+++ alex/tests/test_numpy_with_optimised_ATLAS.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python
 
 
-print "=" * 120
-print " If you can see that all your cores are running at 100% then you are using optimised ATLAS library."
-print "=" * 120
-print
+def main():
+    print "=" * 120
+    print " If you can see that all your cores are running at 100% then you are using optimised ATLAS library."
+    print "=" * 120
+    print
 
-import numpy
-#numpy.test()   #this should run with no errors (skipped tests and known-fails are ok)
+    import numpy
+# numpy.test()   #this should run with no errors (skipped tests and known-fails are ok)
 
-size = 8000
+    size = 8000
 
-if id(numpy.dot) == id(numpy.core.multiarray.dot):
-    # A way to know if you use fast blas/lapack or not. However, it wont tell you whetehr it is generric ATLAS or machine optimised version.
-    print "Not using blas/lapack!"
+    if id(numpy.dot) == id(numpy.core.multiarray.dot):
+        # A way to know if you use fast blas/lapack or not. However, it wont tell
+        # you whetehr it is generric ATLAS or machine optimised version.
+        print "Not using blas/lapack!"
 
-print "creating matrix"
-a = numpy.random.randn(size, size)
+    print "creating matrix"
+    a = numpy.random.randn(size, size)
 
-print "multiplying matrix"
-numpy.dot(a.T, a)
+    print "multiplying matrix"
+    numpy.dot(a.T, a)
 
-print "adding identity matrix"
-i = numpy.identity(size)
-a += i
+    print "adding identity matrix"
+    i = numpy.identity(size)
+    a += i
 
-print "inverting matrix"
-inva = numpy.linalg.inv(a)
+    print "inverting matrix"
+    inva = numpy.linalg.inv(a)
+
+
+if __name__ == '__main__':
+    main()
