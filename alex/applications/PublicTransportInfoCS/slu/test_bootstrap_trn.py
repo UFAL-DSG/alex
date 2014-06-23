dc9196a0ffb9c519210dc7ddc8417111a79d9510
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2014-06-23 09:33:57 +0200

    Calibrated and 100 best list SLU works better, then previous SLU.

--- alex/applications/PublicTransportInfoCS/slu/test_bootstrap_trn.py
+++ alex/applications/PublicTransportInfoCS/slu/test_bootstrap_trn.py
@@ -9,9 +9,9 @@ from alex.applications.PublicTransportInfoCS.slu.test import trained_slu_test, h
 from alex.components.asr.utterance import Utterance
 
 def main():
-    trained_slu_test('./dailogreg.nbl.model.all', './bootstrap.trn', Utterance, './bootstrap.sem')
-    trained_slu_test('./dailogreg.asr.model.all', './bootstrap.trn', Utterance, './bootstrap.sem')
-    trained_slu_test('./dailogreg.trn.model.all', './bootstrap.trn', Utterance, './bootstrap.sem')
+    trained_slu_test('./dailogreg.nbl.model', './bootstrap.trn', Utterance, './bootstrap.sem')
+    trained_slu_test('./dailogreg.asr.model', './bootstrap.trn', Utterance, './bootstrap.sem')
+    trained_slu_test('./dailogreg.trn.model', './bootstrap.trn', Utterance, './bootstrap.sem')
 
 if __name__ == '__main__':
     main()
--- alex/applications/PublicTransportInfoCS/slu/test_trn.py
+++ alex/applications/PublicTransportInfoCS/slu/test_trn.py
@@ -10,6 +10,7 @@ from alex.components.asr.utterance import Utterance, UtteranceNBList
 
 if __name__ == "__main__":
     # cheating experiment on all data using models trained on all data
+    # though these models are used for live system
     trained_slu_test('./dailogreg.trn.model.all', './all.trn', Utterance, './all.trn.hdc.sem')
     trained_slu_test('./dailogreg.asr.model.all', './all.asr', Utterance, './all.trn.hdc.sem')
     trained_slu_test('./dailogreg.nbl.model.all', './all.nbl', UtteranceNBList, './all.trn.hdc.sem')
@@ -19,26 +20,26 @@ if __name__ == "__main__":
     # therefore meaningful results can be only obtained using N-fold cross validation
 
     trained_slu_test('./dailogreg.trn.model', './dev.trn', Utterance, './dev.trn.hdc.sem')
-    trained_slu_test('./dailogreg.trn.model', './dev.asr', Utterance, './dev.trn.hdc.sem')
-    trained_slu_test('./dailogreg.trn.model', './dev.nbl', UtteranceNBList, './dev.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.trn.model', './dev.asr', Utterance, './dev.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.trn.model', './dev.nbl', UtteranceNBList, './dev.trn.hdc.sem')
 
     trained_slu_test('./dailogreg.trn.model', './test.trn', Utterance, './test.trn.hdc.sem')
-    trained_slu_test('./dailogreg.trn.model', './test.asr', Utterance, './test.trn.hdc.sem')
-    trained_slu_test('./dailogreg.trn.model', './test.nbl', UtteranceNBList, './test.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.trn.model', './test.asr', Utterance, './test.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.trn.model', './test.nbl', UtteranceNBList, './test.trn.hdc.sem')
 
 
-    trained_slu_test('./dailogreg.asr.model', './dev.trn', Utterance, './dev.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.asr.model', './dev.trn', Utterance, './dev.trn.hdc.sem')
     trained_slu_test('./dailogreg.asr.model', './dev.asr', Utterance, './dev.trn.hdc.sem')
-    trained_slu_test('./dailogreg.asr.model', './dev.nbl', UtteranceNBList, './dev.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.asr.model', './dev.nbl', UtteranceNBList, './dev.trn.hdc.sem')
 
-    trained_slu_test('./dailogreg.asr.model', './test.trn', Utterance, './test.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.asr.model', './test.trn', Utterance, './test.trn.hdc.sem')
     trained_slu_test('./dailogreg.asr.model', './test.asr', Utterance, './test.trn.hdc.sem')
-    trained_slu_test('./dailogreg.asr.model', './test.nbl', UtteranceNBList, './test.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.asr.model', './test.nbl', UtteranceNBList, './test.trn.hdc.sem')
 
-    trained_slu_test('./dailogreg.nbl.model', './dev.trn', Utterance, './dev.trn.hdc.sem')
-    trained_slu_test('./dailogreg.nbl.model', './dev.asr', Utterance, './dev.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.nbl.model', './dev.trn', Utterance, './dev.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.nbl.model', './dev.asr', Utterance, './dev.trn.hdc.sem')
     trained_slu_test('./dailogreg.nbl.model', './dev.nbl', UtteranceNBList, './dev.trn.hdc.sem')
 
-    trained_slu_test('./dailogreg.nbl.model', './test.trn', Utterance, './test.trn.hdc.sem')
-    trained_slu_test('./dailogreg.nbl.model', './test.asr', Utterance, './test.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.nbl.model', './test.trn', Utterance, './test.trn.hdc.sem')
+#    trained_slu_test('./dailogreg.nbl.model', './test.asr', Utterance, './test.trn.hdc.sem')
     trained_slu_test('./dailogreg.nbl.model', './test.nbl', UtteranceNBList, './test.trn.hdc.sem')
