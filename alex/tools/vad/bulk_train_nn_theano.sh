3547637d4d8a74ab6754a85abeed3655a719a489
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2014-07-14 13:27:49 +0200

    Setting maximum NN VAD epoch for training.

--- alex/tools/vad/bulk_train_nn_theano.sh
+++ alex/tools/vad/bulk_train_nn_theano.sh
@@ -2,23 +2,25 @@
 ## Fail: python.sh
 #
 
-for M in sg-fixedlr sg-rprop sg-adalr
+for M in sg-rprop
 do
     for A in tanh sigmoid softplus relu
     do
-        #nohup ./train_voip_en_nn_theano_sds_mfcc.py --method $M --hact $A --batch_size 20000 --max_frames 1000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1k_hu_32_lf_0_c0_0.$M.$A &
-        nohup ./train_voip_en_nn_theano_sds_mfcc.py --method $M --hact $A --batch_size 20000 --max_frames 1000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1M_hu_32_lf_0_c0_0.$M.$A &
+        #nohup ./train_voip_en_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 20000 --max_frames 1000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1k_hu_32_lf_0_c0_0.$M.$A &
+        #nohup ./train_voip_en_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 20000 --max_frames 1000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1M_hu_32_lf_0_c0_0.$M.$A &
+        nohup ./train_voip_en_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 100000 --max_frames 100000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_100M_hu_32_lf_0_c0_0.$M.$A &
         echo "." && sleep 20
     done
     wait
 done
 
-for M in ng-fixedlr ng-rprop ng-adalr
+for M in ng-fixedlr
 do
     for A in tanh sigmoid softplus relu
     do
-        #nohup ./train_voip_en_nn_theano_sds_mfcc.py --method $M --hact $A --batch_size 20000 --max_frames 1000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1k_hu_32_lf_0_c0_0.$M.$A &
-        nohup ./train_voip_en_nn_theano_sds_mfcc.py --method $M --hact $A --batch_size 20000 --max_frames 1000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1M_hu_32_lf_0_c0_0.$M.$A &
+        #nohup ./train_voip_en_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 20000 --max_frames 1000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1k_hu_32_lf_0_c0_0.$M.$A &
+        #nohup ./train_voip_en_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 20000 --max_frames 1000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1M_hu_32_lf_0_c0_0.$M.$A &
+        nohup ./train_voip_en_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 100000 --max_frames 100000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_100M_hu_32_lf_0_c0_0.$M.$A &
         echo "." && sleep 20
         wait
     done
