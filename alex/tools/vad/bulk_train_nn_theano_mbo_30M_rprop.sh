385e85c52ea16edb9def8060f7688b882ec7b45e
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2014-08-05 09:50:33 +0200

    New version of FFNN VAD.

---
+++ alex/tools/vad/bulk_train_nn_theano_mbo_30M_rprop.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+## Fail: python.sh
+#
+
+DEV='gpu'
+EP='1000'
+MF='30000000'
+MFS='30M'
+
+BS='200000'
+BSS='200k'
+
+LF=0
+PF=0
+
+M='sg-rprop'
+A='tanh'
+
+export THEANO_FLAGS=mode=FAST_RUN,device=$DEV,floatX=float32
+nohup  ./train_voip_en_nn_theano_sds_mfcc.py --max_epoch $EP --method $M --hact $A --batch_size $BS --max_frames $MF --hidden_units 32 --last_frames $LF --prev_frames $PF --mel_banks_only 1 > log.nnt_mf_$MFS.hu_32_lf_$LF.pf_$PF.mbo.bs_$BSS.$M.$A &
+
+
+
