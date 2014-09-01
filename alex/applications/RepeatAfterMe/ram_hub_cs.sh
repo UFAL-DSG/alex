45713dab12ece55f4fefbe416a0c5659353cc14b
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2014-09-01 09:26:37 +0200

    Added exit into RAM after each call.

--- alex/applications/RepeatAfterMe/ram_hub_cs.sh
+++ alex/applications/RepeatAfterMe/ram_hub_cs.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-./ram_hub.py -c ram_hub_cs.cfg ../../resources/private/ext-ram-277278110.cfg
+for A in {1..1000}
+do
+    ./ram_hub.py -c ram_hub_cs.cfg ../../resources/private/ext-ram-277278110.cfg
+done
+
 
