ceb80f23d5c18fe8a8e2c41f2342d89900f15849
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2014-10-07 14:44:02 +0200

    Renaming VAD scripts.

--- alex/tools/vad/train_vad_nn_theano.sh
+++ alex/tools/vad/train_vad_nn_theano.sh
@@ -8,7 +8,7 @@ do
     do
         #nohup ./train_voip_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 10000 --max_frames 1000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1k_hu_32_lf_0_c0_0.$M.$A &
         #nohup ./train_voip_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 10000 --max_frames 1000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1M_hu_32_lf_0_c0_0.$M.$A &
-        nohup ./train_voip_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 10000 --max_frames 100000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_100M_hu_32_lf_0_c0_0.$M.$A &
+        nohup ./train_vad_nn_theano.py --max_epoch 15 --method $M --hact $A --batch_size 10000 --max_frames 100000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_100M_hu_32_lf_0_c0_0.$M.$A &
         echo "." && sleep 20
     done
     wait
@@ -20,7 +20,7 @@ do
     do
         #nohup ./train_voip_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 10000 --max_frames 1000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1k_hu_32_lf_0_c0_0.$M.$A &
         #nohup ./train_voip_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 10000 --max_frames 1000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_1M_hu_32_lf_0_c0_0.$M.$A &
-        nohup ./train_voip_nn_theano_sds_mfcc.py --max_epoch 15 --method $M --hact $A --batch_size 10000 --max_frames 100000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_100M_hu_32_lf_0_c0_0.$M.$A &
+        nohup ./train_vad_nn_theano.py --max_epoch 15 --method $M --hact $A --batch_size 10000 --max_frames 100000000 --hidden_units 32 --last_frames 0 --usec0 0 > log.nnt_mf_100M_hu_32_lf_0_c0_0.$M.$A &
         echo "." && sleep 20
         wait
     done
