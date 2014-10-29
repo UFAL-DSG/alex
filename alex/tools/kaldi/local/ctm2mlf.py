d178b63ddafe7251cf2adfb77584d426109f6123
Ondrej Platek <ondrej.platek@seznam.cz>
2014-10-29 16:02:14 +0100

    script ctm2mlf finished (todo miliseconds/nanoseconds? in mlf)

--- alex/tools/kaldi/local/ctm2mlf.py
+++ alex/tools/kaldi/local/ctm2mlf.py
@@ -18,12 +18,48 @@ from __future__ import absolute_import, division, unicode_literals, print_functi
 import argparse
 
 
+def write_mlf_recording(wf, name, alignment, second_split=100000):
+    wf.write('"%s"\n' % name)
+    for (start, length, phoneme) in alignment:
+        start = start * second_split
+        end = start + (length * second_split)
+        wf.write('%d %d %s\n' % (start, end, phoneme))
+    wf.write('.\n')
+
+
+def ctm2mlf(ctmr, mlfw):
+    mlfw.write("#!MLF!#\n")
+
+    current_name, alignment = None, []
+    for line in ctmr:
+        split_line = line.strip().split(' ')
+        assert(len(split_line) == 5)
+        name, one, start, length, phoneme = split_line
+        assert(one == '1')
+        start, length = float(start), float(length)
+        if name != current_name:
+            if current_name is not None:
+                write_mlf_recording(mlfw, current_name, alignment)
+            current_name, alignment = name, []
+        alignment.append((start, length, phoneme))
+    if current_name is not None:
+        write_mlf_recording(mlfw, current_name, alignment)
+
+            
+
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Convert alignments in ctm format to mlf format')
-    parser.add_argument('ctm-ali', type=str, action='store',
+        description='''Convert alignments in ctm format to mlf format.
+        The assumption is that you used alex/tools/kaldi/local/get_train_ctm_phones.sh script
+        for generating ctm file and you are generating mlf for estimating vad using
+        alex/tools/vad/train_vad_nn_theano.py''')
+    parser.add_argument('ctmali', type=str, action='store',
                         help='Input alignments in ctm format. See Sclite.')
-    parser.add_argument('mlf-ali', type=str, action='store',
+    parser.add_argument('mlfali', type=str, action='store',
                         help='Onput alignments in HTK mlf format.')
     args = parser.parse_args()
-    print('TODO')
+    with open(args.ctmali, 'r') as ctmr:
+        with open(args.mlfali, 'w') as mlfw:
+            ctm2mlf(ctmr, mlfw)
