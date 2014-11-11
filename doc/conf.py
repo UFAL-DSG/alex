2972acd888bcae30c82fc87cb28b07899ed8b7aa
Lukas Zilka <lukas@zilka.me>
2014-11-11 14:00:41 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(os.path.join(__file__, '..')))
-print "XXXXXXX", sys.path
+print "XXXXXXX", os.listdir(os.path.abspath(os.path.join(__file__, '..')))
 
 # -- General configuration -----------------------------------------------------
 
