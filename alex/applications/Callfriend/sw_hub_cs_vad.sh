8351215ecd28036821a5aeca0106c8451bd1ab4f
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2015-02-16 11:46:34 +0100

    Updateing callfriend app.

--- alex/applications/Callfriend/sw_hub_cs_vad.sh
+++ alex/applications/Callfriend/sw_hub_cs_vad.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./sw_hub.py -o sw_hub_cs1.cfg ../../resources/private/ext-ram-277278110.cfg vad.cfg -d sw_hub_cs2.cfg ../../resources/private/ext-sw-277278112.cfg vad.cfg
+./sw_hub.py -o sw_hub_cs1.cfg ../../resources/private/ext-sw-277278111.cfg vad.cfg -d sw_hub_cs2.cfg ../../resources/private/ext-sw-277278112.cfg vad.cfg
 
