c87817b0f4bcc16bd090d899c7dcdc12888bc72c
Martin Vejman <vejmanm@ssakhk.cz>
2015-03-06 21:49:48 +0100

    vystadial upload data

--- alex/applications/PublicTransportInfoEN/data/upload_data.sh
+++ alex/applications/PublicTransportInfoEN/data/upload_data.sh
@@ -1,1 +1,13 @@
-scp stops.txt cities_stops.tsv idos_map.tsv stops.expanded.txt cities.expanded.txt vystadial.ms.mff.cuni.cz:/var/www/download/alex/applications/PublicTransportInfoEN/data
+scp cities.expanded.txt \
+    cities.locations.csv \
+    boroughs.expanded.txt \
+    states.expanded.txt \
+    stops.borough.locations.csv \
+    stops.expanded.txt \
+    stops.locations.csv \
+    streets.expanded.txt \
+    streets.types.csv \
+    vystadial.ms.mff.cuni.cz:/var/www/download/alex/applications/PublicTransportInfoEN/data
+
+scp preprocessing/resources/* \
+    vystadial.ms.mff.cuni.cz:/var/www/download/alex/applications/PublicTransportInfoEN/data/preprocessing/resources/
