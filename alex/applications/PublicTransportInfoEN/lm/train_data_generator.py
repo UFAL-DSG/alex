0151199e3b4e6d2df16fb340298a1421229de8ee
Filip Jurcicek <jurcicek@ufal.mff.cuni.cz>
2015-04-09 10:38:34 +0200

    Added support for autopath

--- alex/applications/PublicTransportInfoEN/lm/train_data_generator.py
+++ alex/applications/PublicTransportInfoEN/lm/train_data_generator.py
@@ -1,4 +1,10 @@
-from __future__ import absolute_import, unicode_literals
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+if __name__ == '__main__':
+    import autopath
+
 import codecs
 
 from datetime import datetime
