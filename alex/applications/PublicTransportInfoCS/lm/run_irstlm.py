ca3dcff7c2a4f43906aeb95279225975f913e6ac
Ondrej Platek <ondrej.platek@seznam.cz>
2015-06-16 13:44:25 +0200

    very basic script working
    TODO1: Limit expanding multiple classes by e.g. by max(|C1|, |C2|, ..,|Cm|)
    TODO2: Compute partial counts and convert to int num in ngrams_discard_classes

--- alex/applications/PublicTransportInfoCS/lm/run_irstlm.py
+++ alex/applications/PublicTransportInfoCS/lm/run_irstlm.py
@@ -4,12 +4,14 @@
 Building LM with irstlm
 """
 from __future__ import division, unicode_literals
+if __name__ == '__main__':
+    import autopath
+
 from subprocess import call
 import os
 from os.path import isfile
+from build import exit_on_system_fail
 
-if __name__ == '__main__':
-    import autopath
 
 from alex.corpustools.replace_with_classes import main as replace_with_classes_main
 from alex.corpustools.ngrams_discard_classes import main as ngrams_discard_classes
@@ -28,21 +30,31 @@ def setup_irstlm_env():
     call("build-lm.sh")  # test that IRSLM binaries are callable and in PATH
     return
 
-norm_data = "input.txt"
-classes = "../data/database_SRILM_classes.txt"
 indomain_data_text_trn_norm_cls_classes             = "07_indomain_data_trn_norm_cls.classes"
+indomain_data_text_trn_norm_cls_pg_arpa             = "07_indomain_data_trn_norm_cls.pg.arpa"
+indomain_data_text_trn_norm_cls_pg_arpa             = "07_indomain_data_trn_norm_cls.pg.arpa"
+indomain_data_text_dev_norm                         = "05_indomain_data_dev_norm.txt"
+indomain_data_text_trn_norm                         = "04_indomain_data_trn_norm.txt"
+norm_data = indomain_data_text_trn_norm
+norm_data = "input.txt"  # TODO debug
+classes = "../data/database_SRILM_classes.txt"
 norm_data_class = "normalized_with_classes"
-norm_data_class_se = "normalized_with_classes_se"
+norm_data_class_se = norm_data_class + "_se"
 class_ngrams_n = 3
 class_ngrams = "class_ngram_counts"
 norm_data_ngrams_estim_classes = "norm_data_ngrams_estim_classes"
 class_arpa = "class_arpa"
 norm_data_se = norm_data + '_se'
-######################### script start #############################
+plain_ngrams = "plain_ngram_counts"
+plain_arpa = "plain_arpa"
 
+
+######################### script start #############################
+print 'Estimating classed based models with expanded classes\n'
 if (not isfile(indomain_data_text_trn_norm_cls_classes)) or (not isfile(norm_data_class)):
-    replace_with_classes_main(norm_data, classes,
-            indomain_data_text_trn_norm_cls_classes, norm_data_class, add_n_smoothing=0, counts_only=True)
+    replace_with_classes_main(norm_data, classes, 
+            indomain_data_text_trn_norm_cls_classes, norm_data_class, 
+            add_n_smoothing=0, counts_only=True)
 
 if not isfile(norm_data_class_se):
     s('cat %s | add-start-end.sh > %s' % (norm_data_class, norm_data_class_se))
@@ -54,13 +66,55 @@ if not isfile(norm_data_ngrams_estim_classes):
     ngrams_discard_classes(class_ngrams, class_ngrams_n, indomain_data_text_trn_norm_cls_classes, norm_data_ngrams_estim_classes, force_integer_counts=True, class_smooth=10)
 
 if not isfile(class_arpa):
-    print 'debug 0'
-    s('tlm -tr=%s -n=%d -lm=LinearWittenBell -oarpa=%s' % (class_ngrams, class_ngrams_n, class_arpa))
-    print '\ndebug 1'
-    # s('tlm -tr=%s -n=%d -lm=LinearWittenBell -oarpa=%s' % (norm_data_ngrams_estim_classes, class_ngrams_n, class_arpa))
+    s('tlm -tr=%s -n=%d -lm=LinearWittenBell -oarpa=%s' % (norm_data_ngrams_estim_classes, class_ngrams_n, class_arpa))
 
 
-# # perplexity on the training data
+print 'Estimating plain model\n'
+if not isfile(norm_data_se):
+    s('cat %s | add-start-end.sh > %s' % (norm_data, norm_data_se))
+if not isfile(plain_ngrams):
+    s('ngt -i=%s -n=%d, -o=%s' % (norm_data_se, class_ngrams_n, plain_ngrams))
+if not isfile(plain_arpa):
+    s('tlm -tr=%s -n=%d -lm=LinearWittenBell -oarpa=%s' % (plain_ngrams, class_ngrams_n, plain_arpa))
+
+# # perplexity on the training data using IRSTLM
 # if not isfile(norm_data_se):
 #     s('cat %s | add-start-end.sh > %s' % (norm_data, norm_data_se))
 # s('compile-lm %s --eval=%s' % (class_arpa, norm_data_se))
+
+from sys import exit; exit(0)  # TODO debug I have to install SRILM
+
+print "Test language models TOD still using SRILM"
+
+print "-"*120
+print "Class-based NEW approach trn 5-gram LM on trn data."
+print "-"*120
+exit_on_system_fail("ngram -lm %s -order 5 -ppl %s" % (class_arpa, indomain_data_text_trn_norm))
+
+print "-"*120
+print "Class-based NEW approach trn 5-gram LM on dev data."
+print "-"*120
+exit_on_system_fail("ngram -lm %s -order 5 -ppl %s" % (class_arpa, indomain_data_text_dev_norm))
+
+print "-"*120
+print "Non Class-based trn 5-gram LM on trn data."
+print "-"*120
+exit_on_system_fail("ngram -lm %s -order 5 -ppl %s" % (plain_arpa, indomain_data_text_trn_norm))
+print "-"*120
+print "Non Class-based trn 5-gram LM on dev data."
+print "-"*120
+exit_on_system_fail("ngram -lm %s -order 5 -ppl %s" % (plain_arpa, indomain_data_text_dev_norm))
+
+print "-"*120
+print "Class-based trn 5-gram LM on trn data."
+print "-"*120
+exit_on_system_fail("ngram -lm %s -classes %s -order 5 -ppl %s" % (indomain_data_text_trn_norm_cls_pg_arpa,
+                                                         indomain_data_text_trn_norm_cls_classes,
+                                                         indomain_data_text_trn_norm))
+print "-"*120
+print "Class-based trn 5-gram LM on dev data."
+print "-"*120
+exit_on_system_fail("ngram -lm %s -classes %s -order 5 -ppl %s -zeroprob-word _NOISE_" % (indomain_data_text_trn_norm_cls_pg_arpa,
+                                                         indomain_data_text_trn_norm_cls_classes,
+                                                         indomain_data_text_dev_norm))
+print
--- alex/corpustools/ngrams_discard_classes.py
+++ alex/corpustools/ngrams_discard_classes.py
@@ -14,8 +14,12 @@
 # MERCHANTABLITY OR NON-INFRINGEMENT.
 # See the Apache 2 License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import unicode_literals, division
 import codecs
 from collections import defaultdict
+from itertools import izip
+from functools import reduce
+import operator
 
 
 def class_prob_file2dict(filename):
@@ -28,46 +32,69 @@ def class_prob_file2dict(filename):
 
 
 def gen_sublist(l, n):
+    '''TODO Generator of substrings of length n.
+    Assuming input strings & output strings.'''
     res = []
     for i in range(len(l) - n + 1):
         res.append(l[i:i+n])
     return res
 
 
-def expand_ngram(words, count, ngram_order, cls_dict, force_ints, class_smooth):
-    expws = [[]]
-    for w in words:
-        if w in cls_dict:
-            last_w_alt = cls_dict[w]
-            for last_w, prob in last_w_alt.iteritems():
-                for expw in expws:
-                    expw.append((last_w, prob))
-        else:
-            for expw in expws:
-                expw.append((w, 1.0))
-    expanded_ngrams = []
-    for expw in expws:
-        ws = [w for w, _ in expw]
-        coef = sum([c for _, c in expw])
+def expand_classes(list_of_list_of_tuples):
+    """TODO Generator of tuples from [cl1, cl2, cl3]"""
+    if len(list_of_list_of_tuples) == 0:
+        yield (), 1.0
+    else:
+        indexes = [0] * len(list_of_list_of_tuples)
+        num_items = [len(d) for d in list_of_list_of_tuples]
+        assert all([n > 0 for n in num_items])
+        finished = False
+        while not finished:
+            class_prob_pairs = [klist[i] for klist, i in izip(list_of_list_of_tuples, indexes)]
+            t = tuple([c for c, _ in class_prob_pairs])
+            coef = sum([c for _, c in class_prob_pairs])
+            yield t, coef
+            indexes[0] += 1
+            overflow_check, i = True, 0
+            while overflow_check:
+                if indexes[i] == num_items[i]:
+                    if i == len(indexes) - 1:
+                        assert(indexes[-1] == num_items[-1])
+                        finished = True
+                        break
+                    indexes[i], indexes[i + 1], i = 0, indexes[i + 1] + 1, i + 1
+                else:
+                    overflow_check = False
+
+
+def expand_ngram(words, count, ngram_order, cls_dict, force_ints, class_smooth, output):
+    # TODO compute in advance the number of candidates and limit it to something reasonable
+    # TODO prune the low probably ngrams - DO NOT even generate them
+    wrt_string = '%s %d\n' if force_ints else '%s %f\n'
+    classes_in_words = [cls_dict[w].items() for w in words if w in cls_dict]  # TODO change na iteritems
+    classes_size = [len(c) for c in classes_in_words]
+    num_alternative = count * reduce(operator.mul, classes_size , 1)
+    min_count = (count / num_alternative) + class_smooth
+
+    template = ' '.join(['%s' if w in cls_dict else w for w in words])  # usage: template % tuple([cl1, cl2, cl3, ..])
+    for expanded_tuple, prob in expand_classes(classes_in_words):
+        exp_ngram = template % expanded_tuple
+        exp_ngram = exp_ngram.split()
         if class_smooth > 0:
-            coef += class_smooth
-        if len(ws) > ngram_order:
-            for sub in gen_sublist(ws, ngram_order):
-                expanded_ngrams.append((sub, coef)) 
+            prob += class_smooth
+        if len(exp_ngram) > ngram_order:
+            for sub in gen_sublist(exp_ngram, ngram_order):
+                output.write(wrt_string % (' '.join(sub), prob))
         else:
-            expanded_ngrams.append((ws, coef))
-    if force_ints:
-        pass  # TODO I do not to need to use it if I won't be using fractional counts in the first place in replace_with_classes
-    return expanded_ngrams
+            output.write(wrt_string % (' '.join(exp_ngram), prob))
 
 
-def main(ngram_file, ngram_order, class_file, out_ngram_no_classes, 
+def main(ngram_file, ngram_order, class_file, out_ngram_no_classes,
         force_integer_counts=False,
         class_smooth=0,
-        skip_first_lines=1,
+        skip_first_lines=2,
         ):
     cls_dict = class_prob_file2dict(class_file)
-    wrt_string = '%s %d\n' if force_integer_counts else '%s %f\n'
     with codecs.open(ngram_file, 'r', 'utf-8') as r:
         with codecs.open(out_ngram_no_classes, 'w', 'utf-8') as w:
             for i, line in enumerate(r):
@@ -75,10 +102,9 @@ def main(ngram_file, ngram_order, class_file, out_ngram_no_classes,
                     continue
                 line_arr = line.strip().split()
                 ngram, count = line_arr[:-1], float(line_arr[-1])
-                ex = expand_ngram(ngram, count, ngram_order, cls_dict, 
-                    force_integer_counts, class_smooth)
-                for ws, c in ex:
-                    w.write(wrt_string % (' '.join(ws), c))
+                # import ipdb; ipdb.set_trace()
+                ex = expand_ngram(ngram, count, ngram_order, cls_dict,
+                    force_integer_counts, class_smooth, w)
 
 
 if __name__ == '__main__':
@@ -94,6 +120,6 @@ if __name__ == '__main__':
         This option force the counts to be integers again""")
     parser.add_argument('-c', '--add-n-smoothing-for-classes', type=int, default=0)
     args = parser.parse_args()
-    main(parser.input_file, parser.ngram_order, parser.class_file, 
+    main(parser.input_file, parser.ngram_order, parser.class_file,
             parser.output_file, force_integer_counts=parser.force_integer_counts,
             class_smooth=parser.add_n_smoothing_for_classes)
