5d40731f4632cda717e90962a44d179af5f624ec
Lukas Zilka <lukas@zilka.me>
2015-11-30 17:56:28 +0100

    Fixing wshub.

--- alex/components/hub/voiceio.py
+++ alex/components/hub/voiceio.py
@@ -38,6 +38,7 @@ class VoiceIO(object):
     def update_current_utterance_id(self, utt_id):
         print 'curr utterance id', self.curr_utt, 'new:', utt_id
 
+
         if self.curr_utt != -1:
             self._log_audio()
 
