3a1f02b6f2d7fa69644ee259fc000685856c5fe6
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2015-12-03 18:41:10 +0100

    Making task assignment work in the CF job

--- alex/tools/crowdflower/CFJOB-PTIEN.js
+++ alex/tools/crowdflower/CFJOB-PTIEN.js
@@ -1,43 +1,45 @@
-﻿// vthis part is for obtaining location to show warning about eligibility of contributor
-google.load("search", "1",{callback: getLocation});
+// this part is for obtaining location to show warning about eligibility of contributor
 var glocation = "";
 function getLocation() {
-  if (google.loader.ClientLocation) {
-    glocation += google.loader.ClientLocation.address.country_code;
-    glocation += ":";
-    glocation += google.loader.ClientLocation.address.region;
-    glocation += ":";
-    glocation += google.loader.ClientLocation.address.city;
-  }
-  
-  if (glocation.lastIndexOf('US', 0) != 0) {
-    document.getElementById("locationWarning").innerHTML = "This Job can be completed only by native speakers of English from USA. If it is found that this Job was completed from other location than USA then it will be automatically rejected.";
-  }
+    if (google.loader.ClientLocation) {
+        glocation += google.loader.ClientLocation.address.country_code;
+        glocation += ":";
+        glocation += google.loader.ClientLocation.address.region;
+        glocation += ":";
+        glocation += google.loader.ClientLocation.address.city;
+    }
+
+    if (glocation.lastIndexOf('US', 0) !== 0) {
+        document.getElementById("locationWarning").innerHTML = "This Job can be completed only by "
+            + "native speakers of English from USA. If it is found that this Job was completed from "
+            + "other location than USA, then it will be automatically rejected.";
+    }
 }
+google.load("search", "1",{callback: getLocation});
 
 // this part is for calling synchronously my own web server
 var success = false;
-var isIE8 = window.XDomainRequest ? true : false;
-var invocation = createCrossDomainRequest();
+var lastResponse = null;
 
+// browser-independent cross-domain AJAX request
 function createCrossDomainRequest(url, handler) {
-    var request;
-    if (isIE8) {
-        request = new window.XDomainRequest();
+    if (window.XDomainRequest) { // IE8
+        return new window.XDomainRequest();
     } else {
-        request = new XMLHttpRequest();
+        return new XMLHttpRequest();
     }
-    return request;
 }
+var invocation = createCrossDomainRequest();
+var server_url = 'vystadial.ms.mff.cuni.cz:4443';
 
 function callOtherDomain(value) {
     if (success)  // end early
         return;
-    var url = 'https://147.251.253.9/?q=' + value;
+    var url = 'https://' + server_url + '/?q=' + value;
     if (invocation) {
-        if (isIE8) {
+        if (window.XDomainRequest) { // IE8
             invocation.onload = outputResult;
-            // false here makes the call synchronous!
+            // false here makes the call synchronous
             invocation.open("GET", url, false);
             invocation.send();
         } else {
@@ -61,16 +63,17 @@ function handler(evtXHR) {
     }
 }
 
+// check 'success' (if response is 'yes' for a valid code)
+// store last response in the lastResponse variable
 function outputResult() {
     var response = invocation.responseText;
-    json = JSON.parse(response)
+    json = JSON.parse(response);
     if (json.response == "yes")
         success = true;
+    lastResponse = json.response;
 }
 
-
-
-// This block if/else block is used to hijack the functionality of an existing validator (specifically: yext_no_international_url)
+// This if/else block is used to hijack the functionality of an existing validator (specifically: yext_no_international_url)
 if (!_cf_cml.digging_gold) {
     CMLFormValidator.addAllThese([
         ['yext_no_international_url', {
@@ -79,11 +82,12 @@ if (!_cf_cml.digging_gold) {
             },
             validate: function(element, props) {
                 // METHOD_TO_VALIDATE must return true or false
-                return METHOD_TO_VALIDATE(element)
+                return METHOD_TO_VALIDATE(element);
             }
         }]
     ]);
-} else {
+}
+else {
     CMLFormValidator.addAllThese([
         ['yext_no_international_url', {
             validate: function(element, props) {
@@ -100,3 +104,33 @@ function METHOD_TO_VALIDATE(element) {
     callOtherDomain(value);
     return success;
 }
+
+// This is for loading a random task from the server on page load
+function addOnloadEvent(fnc){
+    if ( typeof window.addEventListener != "undefined" )
+        window.addEventListener( "load", fnc, false );
+    else if ( typeof window.attachEvent != "undefined" ) {
+        window.attachEvent( "onload", fnc );
+    }
+    else {
+        if ( window.onload !== null ) {
+            var oldOnload = window.onload;
+            window.onload = function ( e ) {
+                oldOnload( e );
+                window[fnc]();
+            };
+        }
+        else
+            window.onload = fnc;
+    }
+}
+
+function load_task(){
+    callOtherDomain('task');
+    if (lastResponse !== null){
+        var task = document.getElementById('task');
+        task.innerHTML = '<strong>' + lastResponse + '</strong>';
+    }
+}
+
+addOnloadEvent(load_task);
